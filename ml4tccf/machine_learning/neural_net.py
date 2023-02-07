deb761bf594aef6a76225a961e06feffc858e746
thunderhoser <lagerqui@ualberta.ca>
2023-02-07 16:05:02 -0700

    Trying to fix more stupid bugs.

--- ml4tccf/machine_learning/neural_net.py
+++ ml4tccf/machine_learning/neural_net.py
@@ -18,13 +18,19 @@ from ml4tccf.machine_learning import custom_losses
 
 METRIC_FUNCTION_LIST = [
     custom_losses.mean_distance_kilometres(),
-    custom_losses.mean_squared_distance_kilometres()
+    custom_losses.mean_squared_distance_kilometres2(),
+    custom_losses.mean_prediction(),
+    custom_losses.mean_target(),
+    custom_losses.mean_grid_spacing_kilometres()
 ]
 
 METRIC_FUNCTION_DICT = {
     'mean_distance_kilometres': custom_losses.mean_distance_kilometres(),
-    'mean_squared_distance_kilometres':
-        custom_losses.mean_squared_distance_kilometres()
+    'mean_squared_distance_kilometres2':
+        custom_losses.mean_squared_distance_kilometres2(),
+    'mean_prediction': custom_losses.mean_prediction(),
+    'mean_target': custom_losses.mean_target(),
+    'mean_grid_spacing_kilometres': custom_losses.mean_grid_spacing_kilometres()
 }
 
 METRES_TO_KM = 0.001
