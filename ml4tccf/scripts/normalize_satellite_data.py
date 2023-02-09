b52b34b57ecbc2bad6c88952b7e3de19c501e06d
thunderhoser <lagerqui@ualberta.ca>
2023-02-09 14:13:19 -0700

    Changed input args to normalize_satellite_data.py.

--- ml4tccf/scripts/normalize_satellite_data.py
+++ ml4tccf/scripts/normalize_satellite_data.py
@@ -2,13 +2,12 @@
 
 import argparse
 from ml4tccf.io import satellite_io
-from ml4tccf.utils import misc_utils
 from ml4tccf.utils import normalization
 
 SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
 INPUT_DIR_ARG_NAME = 'input_satellite_dir_name'
-YEAR_ARG_NAME = 'year'
+CYCLONE_ID_ARG_NAME = 'cyclone_id_string'
 NORMALIZATION_FILE_ARG_NAME = 'input_normalization_file_name'
 # COMPRESS_ARG_NAME = 'compress_output_files'
 OUTPUT_DIR_ARG_NAME = 'output_satellite_dir_name'
@@ -18,7 +17,7 @@ INPUT_DIR_HELP_STRING = (
     'will be found by `satellite_io.find_file` and read by '
     '`satellite_io.read_file`.'
 )
-YEAR_HELP_STRING = 'Will normalize satellite data from this year.'
+CYCLONE_ID_HELP_STRING = 'Will normalize satellite data for this cyclone.'
 NORMALIZATION_FILE_HELP_STRING = (
     'Path to file with normalization params (will be read by '
     '`normalization.read_file`).'
@@ -35,7 +34,8 @@ INPUT_ARG_PARSER.add_argument(
     help=INPUT_DIR_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
-    '--' + YEAR_ARG_NAME, type=int, required=True, help=YEAR_HELP_STRING
+    '--' + CYCLONE_ID_ARG_NAME, type=int, required=True,
+    help=CYCLONE_ID_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
     '--' + NORMALIZATION_FILE_ARG_NAME, type=str, required=True,
@@ -47,14 +47,14 @@ INPUT_ARG_PARSER.add_argument(
 )
 
 
-def _run(input_satellite_dir_name, year, normalization_file_name,
+def _run(input_satellite_dir_name, cyclone_id_string, normalization_file_name,
          output_satellite_dir_name):
     """Normalizes satellite data.
 
     This is effectively the main method.
 
     :param input_satellite_dir_name: See documentation at top of file.
-    :param year: Same.
+    :param cyclone_id_string: Same.
     :param normalization_file_name: Same.
     :param output_satellite_dir_name: Same.
     """
@@ -64,53 +64,42 @@ def _run(input_satellite_dir_name, year, normalization_file_name,
         normalization_file_name
     )
 
-    cyclone_id_strings = satellite_io.find_cyclones(
-        directory_name=input_satellite_dir_name, raise_error_if_all_missing=True
+    input_satellite_file_names = satellite_io.find_files_one_cyclone(
+        directory_name=input_satellite_dir_name,
+        cyclone_id_string=cyclone_id_string, raise_error_if_all_missing=True
     )
-    cyclone_id_strings = set([
-        c for c in cyclone_id_strings
-        if misc_utils.parse_cyclone_id(c)[0] == year
-    ])
-    cyclone_id_strings = list(cyclone_id_strings)
-    cyclone_id_strings.sort()
+    input_satellite_file_names.sort()
 
-    for this_cyclone_id_string in cyclone_id_strings:
-        input_satellite_file_names = satellite_io.find_files_one_cyclone(
-            directory_name=input_satellite_dir_name,
-            cyclone_id_string=this_cyclone_id_string,
-            raise_error_if_all_missing=True
+    output_satellite_file_names = [
+        satellite_io.find_file(
+            directory_name=output_satellite_dir_name,
+            cyclone_id_string=cyclone_id_string,
+            valid_date_string=satellite_io.file_name_to_date(f),
+            raise_error_if_missing=False
+        )
+        for f in input_satellite_file_names
+    ]
+
+    for i in range(len(input_satellite_file_names)):
+        print('Reading unnormalized data from: "{0:s}"...'.format(
+            input_satellite_file_names[i]
+        ))
+        satellite_table_xarray = satellite_io.read_file(
+            input_satellite_file_names[i]
         )
 
-        output_satellite_file_names = [
-            satellite_io.find_file(
-                directory_name=output_satellite_dir_name,
-                cyclone_id_string=this_cyclone_id_string,
-                valid_date_string=satellite_io.file_name_to_date(f),
-                raise_error_if_missing=False
-            )
-            for f in input_satellite_file_names
-        ]
+        satellite_table_xarray = normalization.normalize_data(
+            satellite_table_xarray=satellite_table_xarray,
+            normalization_param_table_xarray=normalization_param_table_xarray
+        )
 
-        for i in range(len(input_satellite_file_names)):
-            print('Reading unnormalized data from: "{0:s}"...'.format(
-                input_satellite_file_names[i]
-            ))
-            satellite_table_xarray = satellite_io.read_file(
-                input_satellite_file_names[i]
-            )
-
-            satellite_table_xarray = normalization.normalize_data(
-                satellite_table_xarray=satellite_table_xarray,
-                normalization_param_table_xarray=normalization_param_table_xarray
-            )
-
-            print('Writing normalized data to: "{0:s}"...'.format(
-                output_satellite_file_names[i]
-            ))
-            satellite_io.write_file(
-                satellite_table_xarray=satellite_table_xarray,
-                netcdf_file_name=output_satellite_file_names[i]
-            )
+        print('Writing normalized data to: "{0:s}"...'.format(
+            output_satellite_file_names[i]
+        ))
+        satellite_io.write_file(
+            satellite_table_xarray=satellite_table_xarray,
+            netcdf_file_name=output_satellite_file_names[i]
+        )
 
 
 if __name__ == '__main__':
@@ -118,7 +107,7 @@ if __name__ == '__main__':
 
     _run(
         input_satellite_dir_name=getattr(INPUT_ARG_OBJECT, INPUT_DIR_ARG_NAME),
-        year=getattr(INPUT_ARG_OBJECT, YEAR_ARG_NAME),
+        cyclone_id_string=getattr(INPUT_ARG_OBJECT, CYCLONE_ID_ARG_NAME),
         normalization_file_name=getattr(
             INPUT_ARG_OBJECT, NORMALIZATION_FILE_ARG_NAME
         ),
