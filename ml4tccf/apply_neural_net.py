2bc5c1139c2266e3f9e96e166af1adebeea289a6
thunderhoser <lagerqui@ualberta.ca>
2023-02-14 15:48:01 -0700

    FOO

---
+++ ml4tccf/apply_neural_net.py
@@ -0,0 +1,216 @@
+"""Applies trained neural net -- inference time!"""
+
+import os
+import sys
+import argparse
+import numpy
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import error_checking
+import prediction_io
+import neural_net
+
+SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
+
+LARGE_INTEGER = int(1e10)
+NUM_EXAMPLES_PER_BATCH = 32
+
+MODEL_FILE_ARG_NAME = 'input_model_file_name'
+SATELLITE_DIR_ARG_NAME = 'input_satellite_dir_name'
+CYCLONE_ID_ARG_NAME = 'cyclone_id_string'
+NUM_BNN_ITERATIONS_ARG_NAME = 'num_bnn_iterations'
+MAX_ENSEMBLE_SIZE_ARG_NAME = 'max_ensemble_size'
+NUM_TRANSLATIONS_ARG_NAME = 'data_aug_num_translations'
+OUTPUT_DIR_ARG_NAME = 'output_dir_name'
+
+MODEL_FILE_HELP_STRING = (
+    'Path to trained model (will be read by `neural_net.read_model`).'
+)
+SATELLITE_DIR_HELP_STRING = (
+    'Name of directory with satellite (predictor) data.  Files therein will be '
+    'found by `satellite_io.find_file` and read by `satellite_io.read_file`.'
+)
+CYCLONE_ID_HELP_STRING = (
+    'Will apply neural net to data from this cyclone.  Cyclone ID must be in '
+    'format "yyyyBBnn".'
+)
+NUM_BNN_ITERATIONS_HELP_STRING = (
+    '[used only if the model is a Bayesian neural net (BNN)] Number of times '
+    'to run the BNN.'
+)
+MAX_ENSEMBLE_SIZE_HELP_STRING = (
+    'Max ensemble size.  If final ensemble size ends up being larger, it will '
+    'be randomly subset to equal {0:s}.'
+).format(MAX_ENSEMBLE_SIZE_ARG_NAME)
+
+NUM_TRANSLATIONS_HELP_STRING = (
+    'Number of translations for each cyclone snapshot (one snapshot = one '
+    'cyclone at one target time).  Total number of data samples will be '
+    'num_snapshots * {0:s}.'
+).format(NUM_TRANSLATIONS_ARG_NAME)
+
+OUTPUT_DIR_HELP_STRING = (
+    'Name of output directory.  Results will be written by '
+    '`prediction_io.write_file`, to a location in this directory determined by '
+    '`prediction_io.find_file`.'
+)
+
+INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + MODEL_FILE_ARG_NAME, type=str, required=True,
+    help=MODEL_FILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + SATELLITE_DIR_ARG_NAME, type=str, required=True,
+    help=SATELLITE_DIR_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + CYCLONE_ID_ARG_NAME, type=str, required=True,
+    help=CYCLONE_ID_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_BNN_ITERATIONS_ARG_NAME, type=int, required=False, default=1,
+    help=NUM_BNN_ITERATIONS_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + MAX_ENSEMBLE_SIZE_ARG_NAME, type=int, required=False,
+    default=LARGE_INTEGER, help=MAX_ENSEMBLE_SIZE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_TRANSLATIONS_ARG_NAME, type=int, required=True,
+    help=NUM_TRANSLATIONS_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
+    help=OUTPUT_DIR_HELP_STRING
+)
+
+
+def _run(model_file_name, satellite_dir_name, cyclone_id_string,
+         num_bnn_iterations, max_ensemble_size, data_aug_num_translations,
+         output_dir_name):
+    """Applies trained neural net -- inference time!
+
+    This is effectively the main method.
+
+    :param model_file_name: See documentation at top of file.
+    :param satellite_dir_name: Same.
+    :param cyclone_id_string: Same.
+    :param num_bnn_iterations: Same.
+    :param max_ensemble_size: Same.
+    :param data_aug_num_translations: Same.
+    :param output_dir_name: Same.
+    """
+
+    error_checking.assert_is_geq(data_aug_num_translations, 1)
+
+    print('Reading model from: "{0:s}"...'.format(model_file_name))
+    model_object = neural_net.read_model(model_file_name)
+    model_metafile_name = neural_net.find_metafile(
+        model_dir_name=os.path.split(model_file_name)[0],
+        raise_error_if_missing=True
+    )
+
+    print('Reading metadata from: "{0:s}"...'.format(model_metafile_name))
+    model_metadata_dict = neural_net.read_metafile(model_metafile_name)
+
+    if model_metadata_dict[neural_net.BNN_ARCHITECTURE_KEY] is None:
+        num_bnn_iterations = 1
+
+    error_checking.assert_is_geq(num_bnn_iterations, 1)
+
+    validation_option_dict = (
+        model_metadata_dict[neural_net.VALIDATION_OPTIONS_KEY]
+    )
+    validation_option_dict[neural_net.SATELLITE_DIRECTORY_KEY] = (
+        satellite_dir_name
+    )
+    validation_option_dict[neural_net.DATA_AUG_NUM_TRANS_KEY] = (
+        data_aug_num_translations
+    )
+
+    # TODO(thunderhoser): With many target times and many translations, I might
+    # get out-of-memory errors.  In this case, I will write a new version of
+    # this script, which calls `create_data` once without data augmentation and
+    # then augments each example many times.
+    data_dict = neural_net.create_data(
+        option_dict=validation_option_dict, cyclone_id_string=cyclone_id_string,
+        num_target_times=LARGE_INTEGER
+    )
+
+    predictor_matrices = data_dict[neural_net.PREDICTOR_MATRICES_KEY]
+    target_matrix = data_dict[neural_net.TARGET_MATRIX_KEY]
+    target_times_unix_sec = data_dict[neural_net.TARGET_TIMES_KEY]
+
+    prediction_matrix = None
+    inner_ensemble_size = -1
+    print(SEPARATOR_STRING)
+
+    for k in range(num_bnn_iterations):
+        this_prediction_matrix = neural_net.apply_model(
+            model_object=model_object, predictor_matrices=predictor_matrices,
+            num_examples_per_batch=NUM_EXAMPLES_PER_BATCH, verbose=True
+        )
+
+        if prediction_matrix is None:
+            inner_ensemble_size = this_prediction_matrix.shape[-1]
+            total_ensemble_size = inner_ensemble_size * num_bnn_iterations
+            dimensions = (
+                this_prediction_matrix.shape[:-1] + (total_ensemble_size,)
+            )
+            prediction_matrix = numpy.full(dimensions, numpy.nan)
+
+        first_index = k * inner_ensemble_size
+        last_index = first_index + inner_ensemble_size
+        prediction_matrix[..., first_index:last_index] = this_prediction_matrix
+
+        print(SEPARATOR_STRING)
+
+    del predictor_matrices
+
+    total_ensemble_size = prediction_matrix.shape[-1]
+    if total_ensemble_size > max_ensemble_size:
+        ensemble_indices = numpy.linspace(
+            0, total_ensemble_size - 1, num=total_ensemble_size, dtype=int
+        )
+        ensemble_indices = numpy.random.choice(
+            ensemble_indices, size=max_ensemble_size, replace=False
+        )
+        prediction_matrix = prediction_matrix[..., ensemble_indices]
+
+    output_file_name = prediction_io.find_file(
+        directory_name=output_dir_name, cyclone_id_string=cyclone_id_string,
+        raise_error_if_missing=False
+    )
+
+    print('Writing results to: "{0:s}"...'.format(output_file_name))
+    prediction_io.write_file(
+        netcdf_file_name=output_file_name,
+        target_matrix=target_matrix,
+        prediction_matrix=prediction_matrix,
+        cyclone_id_string=cyclone_id_string,
+        target_times_unix_sec=target_times_unix_sec,
+        model_file_name=model_file_name
+    )
+
+
+if __name__ == '__main__':
+    INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
+
+    _run(
+        model_file_name=getattr(INPUT_ARG_OBJECT, MODEL_FILE_ARG_NAME),
+        satellite_dir_name=getattr(INPUT_ARG_OBJECT, SATELLITE_DIR_ARG_NAME),
+        cyclone_id_string=getattr(INPUT_ARG_OBJECT, CYCLONE_ID_ARG_NAME),
+        num_bnn_iterations=getattr(
+            INPUT_ARG_OBJECT, NUM_BNN_ITERATIONS_ARG_NAME
+        ),
+        max_ensemble_size=getattr(INPUT_ARG_OBJECT, MAX_ENSEMBLE_SIZE_ARG_NAME),
+        data_aug_num_translations=getattr(
+            INPUT_ARG_OBJECT, NUM_TRANSLATIONS_ARG_NAME
+        ),
+        output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
+    )
---
+++ ml4tccf/prediction_io.py
@@ -0,0 +1,186 @@
+"""Methods for reading and writing predictions."""
+
+import os
+import sys
+import numpy
+import xarray
+import netCDF4
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import file_system_utils
+import error_checking
+import misc_utils
+
+CYCLONE_ID_KEY = 'cyclone_id_string'
+MODEL_FILE_KEY = 'model_file_name'
+
+EXAMPLE_DIM_KEY = 'example'
+ENSEMBLE_MEMBER_DIM_KEY = 'ensemble_member'
+
+PREDICTED_ROW_OFFSET_KEY = 'predicted_row_offset'
+PREDICTED_COLUMN_OFFSET_KEY = 'predicted_column_offset'
+ACTUAL_ROW_OFFSET_KEY = 'actual_row_offset'
+ACTUAL_COLUMN_OFFSET_KEY = 'actual_column_offset'
+GRID_SPACING_KEY = 'grid_spacing_low_res_km'
+ACTUAL_CENTER_LATITUDE_KEY = 'actual_cyclone_center_latitude_deg_n'
+TARGET_TIME_KEY = 'target_time_unix_sec'
+
+
+def find_file(directory_name, cyclone_id_string, raise_error_if_missing=True):
+    """Finds file with predictions.
+
+    :param directory_name: Name of directory.
+    :param cyclone_id_string: Cyclone ID.
+    :param raise_error_if_missing: Boolean flag.  If file is not found and
+        `raise_error_if_missing == True`, this method will raise an error.  If
+        file is not found and `raise_error_if_missing == False`, this method
+        will return the expected file path.
+    :return: prediction_file_name: Actual or expected file path.
+    :raises: ValueError: if file is not found and
+        `raise_error_if_missing == True`.
+    """
+
+    error_checking.assert_is_string(directory_name)
+    error_checking.assert_is_boolean(raise_error_if_missing)
+    _ = misc_utils.parse_cyclone_id(cyclone_id_string)
+
+    prediction_file_name = '{0:s}/predictions_{1:s}.nc'.format(
+        directory_name, cyclone_id_string
+    )
+
+    if os.path.isfile(prediction_file_name):
+        return prediction_file_name
+
+    if not raise_error_if_missing:
+        return prediction_file_name
+
+    error_string = 'Cannot find prediction file.  Expected at: "{0:s}"'.format(
+        prediction_file_name
+    )
+    raise ValueError(error_string)
+
+
+def write_file(
+        netcdf_file_name, target_matrix, prediction_matrix, cyclone_id_string,
+        target_times_unix_sec, model_file_name):
+    """Writes predictions to NetCDF file.
+
+    E = number of examples
+    S = ensemble size
+
+    :param netcdf_file_name: Path to output file.
+    :param target_matrix: E-by-4 numpy array.  target_matrix[:, 0] contains
+        row positions of TC centers; target_matrix[:, 1] contains column
+        positions of TC centers; target_matrix[:, 2] contains grid spacing in
+        km, used to convert row-column distances to actual distances; and
+        target_matrix[:, 3] contains true TC latitudes in deg north.
+    :param prediction_matrix: E-by-2-by-S numpy array.
+        prediction_matrix[:, 0, :] contains predicted row positions of TC
+        centers, and prediction_matrix[:, 1, :] contains predicted column
+        positions of TC centers.
+    :param cyclone_id_string: Cyclone ID.
+    :param target_times_unix_sec: length-E numpy array of target times.
+    :param model_file_name: Path to trained model (readable by
+        `neural_net.read_model`).
+    """
+
+    # Check input args.
+    error_checking.assert_is_numpy_array(target_matrix, num_dimensions=2)
+    error_checking.assert_is_numpy_array_without_nan(target_matrix)
+    error_checking.assert_equals(target_matrix.shape[1], 4)
+    error_checking.assert_is_greater_numpy_array(target_matrix[:, 2], 0)
+    error_checking.assert_is_valid_lat_numpy_array(target_matrix[:, 3])
+
+    error_checking.assert_is_numpy_array(prediction_matrix, num_dimensions=3)
+    error_checking.assert_is_numpy_array_without_nan(prediction_matrix)
+
+    num_examples = target_matrix.shape[0]
+    ensemble_size = prediction_matrix.shape[2]
+    expected_dim = numpy.array([num_examples, 2, ensemble_size], dtype=int)
+    error_checking.assert_is_numpy_array(
+        prediction_matrix, exact_dimensions=expected_dim
+    )
+
+    _ = misc_utils.parse_cyclone_id(cyclone_id_string)
+
+    error_checking.assert_is_integer_numpy_array(target_times_unix_sec)
+    error_checking.assert_is_numpy_array(
+        target_times_unix_sec,
+        exact_dimensions=numpy.array([num_examples], dtype=int)
+    )
+
+    error_checking.assert_is_string(model_file_name)
+
+    # Do actual stuff.
+    file_system_utils.mkdir_recursive_if_necessary(file_name=netcdf_file_name)
+    dataset_object = netCDF4.Dataset(
+        netcdf_file_name, 'w', format='NETCDF3_64BIT_OFFSET'
+    )
+
+    dataset_object.setncattr(CYCLONE_ID_KEY, cyclone_id_string)
+    dataset_object.setncattr(MODEL_FILE_KEY, model_file_name)
+    dataset_object.createDimension(EXAMPLE_DIM_KEY, num_examples)
+    dataset_object.createDimension(ENSEMBLE_MEMBER_DIM_KEY, ensemble_size)
+
+    dataset_object.createVariable(
+        PREDICTED_ROW_OFFSET_KEY, datatype=numpy.float32,
+        dimensions=(EXAMPLE_DIM_KEY, ENSEMBLE_MEMBER_DIM_KEY)
+    )
+    dataset_object.variables[PREDICTED_ROW_OFFSET_KEY][:] = (
+        prediction_matrix[:, 0, :]
+    )
+
+    dataset_object.createVariable(
+        PREDICTED_COLUMN_OFFSET_KEY, datatype=numpy.float32,
+        dimensions=(EXAMPLE_DIM_KEY, ENSEMBLE_MEMBER_DIM_KEY)
+    )
+    dataset_object.variables[PREDICTED_COLUMN_OFFSET_KEY][:] = (
+        prediction_matrix[:, 1, :]
+    )
+
+    dataset_object.createVariable(
+        ACTUAL_ROW_OFFSET_KEY, datatype=numpy.float32,
+        dimensions=EXAMPLE_DIM_KEY
+    )
+    dataset_object.variables[ACTUAL_ROW_OFFSET_KEY][:] = target_matrix[:, 0]
+
+    dataset_object.createVariable(
+        ACTUAL_COLUMN_OFFSET_KEY, datatype=numpy.float32,
+        dimensions=EXAMPLE_DIM_KEY
+    )
+    dataset_object.variables[ACTUAL_COLUMN_OFFSET_KEY][:] = target_matrix[:, 1]
+
+    dataset_object.createVariable(
+        GRID_SPACING_KEY, datatype=numpy.float32, dimensions=EXAMPLE_DIM_KEY
+    )
+    dataset_object.variables[GRID_SPACING_KEY][:] = target_matrix[:, 2]
+
+    dataset_object.createVariable(
+        ACTUAL_CENTER_LATITUDE_KEY, datatype=numpy.float32,
+        dimensions=EXAMPLE_DIM_KEY
+    )
+    dataset_object.variables[ACTUAL_CENTER_LATITUDE_KEY][:] = (
+        target_matrix[:, 3]
+    )
+
+    dataset_object.createVariable(
+        TARGET_TIME_KEY, datatype=numpy.int32, dimensions=EXAMPLE_DIM_KEY
+    )
+    dataset_object.variables[TARGET_TIME_KEY][:] = target_times_unix_sec
+
+    dataset_object.close()
+
+
+def read_file(netcdf_file_name):
+    """Reads predictions from NetCDF file.
+
+    :param netcdf_file_name: Path to input file.
+    :return: prediction_table_xarray: xarray table.  Variable names and metadata
+        in the table should make it self-explanatory.
+    """
+
+    return xarray.open_dataset(netcdf_file_name)
