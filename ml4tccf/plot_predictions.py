fd3ad044d6beed7a0abd39648be063912f8dd657
thunderhoser <lagerqui@ualberta.ca>
2023-02-21 17:56:49 -0700

    FOO

--- ml4tccf/plot_predictions.py
+++ ml4tccf/plot_predictions.py
@@ -440,7 +440,7 @@ def _run(prediction_file_name, satellite_dir_name, are_data_normalized,
 
     # TODO(thunderhoser): This will not work if I ever have multiple cyclones in
     # one prediction file.
-    cyclone_id_string = pt[prediction_utils.CYCLONE_ID_KEY].values[0]
+    cyclone_id_string = str(pt[prediction_utils.CYCLONE_ID_KEY].values[0])
     target_times_unix_sec = pt[prediction_utils.TARGET_TIME_KEY].values
 
     data_dict = neural_net.create_data_specific_trans(
