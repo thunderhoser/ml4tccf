e52a409ee2803065bafbb584065fa8aec201d266
thunderhoser <lagerqui@ualberta.ca>
2023-03-07 16:59:13 -0700

    FOO

---
+++ ml4tccf/custom_metrics_gridded.py
@@ -0,0 +1,234 @@
+"""Custom metrics for gridded predictions."""
+
+import numpy
+from tensorflow.keras import backend as K
+
+
+def mean_target(target_tensor, prediction_tensor):
+    """Computes mean target value.
+
+    E = number of examples
+    M = number of grid rows
+    N = number of grid columns
+    S = ensemble size
+
+    :param target_tensor: E-by-M-by-N tensor of target values.
+    :param prediction_tensor: E-by-M-by-N-by-S tensor of predicted values.
+    :return: mean_target_value: Scalar.
+    """
+
+    return K.mean(target_tensor)
+
+
+def mean_prediction(target_tensor, prediction_tensor):
+    """Computes mean prediction.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_prediction: Scalar.
+    """
+
+    return K.mean(prediction_tensor)
+
+
+def min_target(target_tensor, prediction_tensor):
+    """Computes minimum target value.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: min_target_value: Scalar.
+    """
+
+    return K.min(target_tensor)
+
+
+def min_prediction(target_tensor, prediction_tensor):
+    """Computes minimum prediction.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: min_prediction: Scalar.
+    """
+
+    return K.min(prediction_tensor)
+
+
+def max_target(target_tensor, prediction_tensor):
+    """Computes maximum target value.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: max_target_value: Scalar.
+    """
+
+    return K.max(target_tensor)
+
+
+def max_prediction(target_tensor, prediction_tensor):
+    """Computes maximum prediction.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: max_prediction: Scalar.
+    """
+
+    return K.max(prediction_tensor)
+
+
+def sum_of_targets(target_tensor, prediction_tensor):
+    """Computes sum of target values.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: sum_of_target_values: Scalar.
+    """
+
+    return K.sum(target_tensor)
+
+
+def sum_of_predictions(target_tensor, prediction_tensor):
+    """Computes sum of predictions.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: sum_of_predictions: Scalar.
+    """
+
+    return K.sum(prediction_tensor)
+
+
+def mean_predictive_stdev(target_tensor, prediction_tensor):
+    """Computes mean standard deviation of predictive distribution.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_predictive_stdev: Scalar.
+    """
+
+    return K.mean(K.std(prediction_tensor, axis=-1))
+
+
+def mean_predictive_range(target_tensor, prediction_tensor):
+    """Computes mean range of predictive distribution.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_predictive_range: Scalar.
+    """
+
+    return K.mean(
+        K.max(prediction_tensor, axis=-1) -
+        K.min(prediction_tensor, axis=-1)
+    )
+
+
+def mean_center_of_mass_row_for_targets(target_tensor, prediction_tensor):
+    """Computes mean row index at center of mass of target grid.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_row_index: Scalar.
+    """
+
+    _, row_index_matrix, column_index_matrix = numpy.indices(
+        target_tensor.shape
+    )
+
+    return K.mean(
+        K.sum(row_index_matrix * target_tensor, axis=(1, 2)) /
+        K.sum(target_tensor, axis=(1, 2))
+    )
+
+
+def mean_center_of_mass_column_for_targets(target_tensor, prediction_tensor):
+    """Computes mean column index at center of mass of target grid.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_column_index: Scalar.
+    """
+
+    _, row_index_matrix, column_index_matrix = numpy.indices(
+        target_tensor.shape
+    )
+
+    return K.mean(
+        K.sum(column_index_matrix * target_tensor, axis=(1, 2)) /
+        K.sum(target_tensor, axis=(1, 2))
+    )
+
+
+def mean_center_of_mass_row_for_predictions(target_tensor, prediction_tensor):
+    """Computes mean row index at center of mass of prediction grid.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_row_index: Scalar.
+    """
+
+    mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+    _, row_index_matrix, column_index_matrix = numpy.indices(
+        mean_prediction_tensor.shape
+    )
+
+    return K.mean(
+        K.sum(row_index_matrix * mean_prediction_tensor, axis=(1, 2)) /
+        K.sum(mean_prediction_tensor, axis=(1, 2))
+    )
+
+
+def mean_center_of_mass_column_for_predictions(target_tensor,
+                                               prediction_tensor):
+    """Computes mean column index at center of mass of prediction grid.
+
+    :param target_tensor: See doc for `mean_target`.
+    :param prediction_tensor: Same.
+    :return: mean_column_index: Scalar.
+    """
+
+    mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+    _, row_index_matrix, column_index_matrix = numpy.indices(
+        mean_prediction_tensor.shape
+    )
+
+    return K.mean(
+        K.sum(column_index_matrix * mean_prediction_tensor, axis=(1, 2)) /
+        K.sum(mean_prediction_tensor, axis=(1, 2))
+    )
+
+
+def mean_center_of_mass_distance_px(target_tensor, prediction_tensor):
+    """Computes mean distance between the two centers of mass.
+
+    :param target_tensor: See doc for
+        `custom_losses_scalar.mean_squared_distance_kilometres2`.
+    :param prediction_tensor: Same.
+    :return: mean_distance_px: Scalar.
+    """
+
+    _, row_index_matrix, column_index_matrix = numpy.indices(
+        target_tensor.shape
+    )
+    target_row_indices = (
+        K.sum(row_index_matrix * target_tensor, axis=(1, 2)) /
+        K.sum(target_tensor, axis=(1, 2))
+    )
+    target_column_indices = (
+        K.sum(column_index_matrix * target_tensor, axis=(1, 2)) /
+        K.sum(target_tensor, axis=(1, 2))
+    )
+
+    mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+    predicted_row_indices = (
+        K.sum(row_index_matrix * mean_prediction_tensor, axis=(1, 2)) /
+        K.sum(mean_prediction_tensor, axis=(1, 2))
+    )
+    predicted_column_indices = (
+        K.sum(column_index_matrix * mean_prediction_tensor, axis=(1, 2)) /
+        K.sum(mean_prediction_tensor, axis=(1, 2))
+    )
+
+    return K.mean(K.sqrt(
+        (target_row_indices - predicted_row_indices) ** 2 +
+        (target_column_indices - predicted_column_indices) ** 2
+    ))
--- ml4tccf/neural_net.py
+++ ml4tccf/neural_net.py
@@ -25,6 +25,7 @@ import image_filtering
 import custom_losses_scalar
 import custom_metrics_scalar
 import custom_losses_gridded
+import custom_metrics_gridded
 import cnn_architecture
 import u_net_architecture
 import accum_grad_optimizer
@@ -105,7 +106,22 @@ GERRITY_FUNCTION = custom_losses_gridded.gerrity_score(
 METRIC_FUNCTION_LIST_GRIDDED = [
     FSS_FUNCTION_1BY1, FSS_FUNCTION_9BY9,
     FSS_FUNCTION_17BY17, FSS_FUNCTION_25BY25,
-    HEIDKE_FUNCTION, PEIRCE_FUNCTION, GERRITY_FUNCTION
+    HEIDKE_FUNCTION, PEIRCE_FUNCTION, GERRITY_FUNCTION,
+    custom_metrics_gridded.mean_target,
+    custom_metrics_gridded.mean_prediction,
+    custom_metrics_gridded.min_target,
+    custom_metrics_gridded.min_prediction,
+    custom_metrics_gridded.max_target,
+    custom_metrics_gridded.max_prediction,
+    custom_metrics_gridded.sum_of_targets,
+    custom_metrics_gridded.sum_of_predictions,
+    custom_metrics_gridded.mean_predictive_stdev,
+    custom_metrics_gridded.mean_predictive_range,
+    custom_metrics_gridded.mean_center_of_mass_row_for_targets,
+    custom_metrics_gridded.mean_center_of_mass_column_for_targets,
+    custom_metrics_gridded.mean_center_of_mass_row_for_predictions,
+    custom_metrics_gridded.mean_center_of_mass_column_for_predictions,
+    custom_metrics_gridded.mean_center_of_mass_distance_px
 ]
 
 METRIC_FUNCTION_DICT_GRIDDED = {
@@ -115,7 +131,27 @@ METRIC_FUNCTION_DICT_GRIDDED = {
     'fss25by25': FSS_FUNCTION_25BY25,
     'heidke_score': HEIDKE_FUNCTION,
     'peirce_score': PEIRCE_FUNCTION,
-    'gerrity_score': GERRITY_FUNCTION
+    'gerrity_score': GERRITY_FUNCTION,
+    'mean_target': custom_metrics_gridded.mean_target,
+    'mean_prediction': custom_metrics_gridded.mean_prediction,
+    'min_target': custom_metrics_gridded.min_target,
+    'min_prediction': custom_metrics_gridded.min_prediction,
+    'max_target': custom_metrics_gridded.max_target,
+    'max_prediction': custom_metrics_gridded.max_prediction,
+    'sum_of_targets': custom_metrics_gridded.sum_of_targets,
+    'sum_of_predictions': custom_metrics_gridded.sum_of_predictions,
+    'mean_predictive_stdev': custom_metrics_gridded.mean_predictive_stdev,
+    'mean_predictive_range': custom_metrics_gridded.mean_predictive_range,
+    'mean_center_of_mass_row_for_targets':
+        custom_metrics_gridded.mean_center_of_mass_row_for_targets,
+    'mean_center_of_mass_column_for_targets':
+        custom_metrics_gridded.mean_center_of_mass_column_for_targets,
+    'mean_center_of_mass_row_for_predictions':
+        custom_metrics_gridded.mean_center_of_mass_row_for_predictions,
+    'mean_center_of_mass_column_for_predictions':
+        custom_metrics_gridded.mean_center_of_mass_column_for_predictions,
+    'mean_center_of_mass_distance_px':
+        custom_metrics_gridded.mean_center_of_mass_distance_px
 }
 
 METRES_TO_KM = 0.001
