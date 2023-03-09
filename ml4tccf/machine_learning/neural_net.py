1f84bd1e16229653572efc6097b9fc96d14d0cbb
thunderhoser <lagerqui@ualberta.ca>
2023-03-09 16:47:01 -0700

    FOO

--- ml4tccf/machine_learning/neural_net.py
+++ ml4tccf/machine_learning/neural_net.py
@@ -73,30 +73,12 @@ METRIC_FUNCTION_DICT_SCALAR = {
 METRIC_FUNCTION_LIST_GRIDDED = []
 METRIC_FUNCTION_DICT_GRIDDED = {}
 
-THESE_HALF_WINDOW_SIZES_PX = numpy.array([0, 3, 6], dtype=int)
-THESE_WINDOW_SIZE_STRINGS = ['1by1', '7by7', '13by13']
+THESE_HALF_WINDOW_SIZES_PX = numpy.array([3, 6], dtype=int)
+THESE_WINDOW_SIZE_STRINGS = ['7by7', '13by13']
 
 for m in range(len(THESE_HALF_WINDOW_SIZES_PX)):
     this_function_name = 'fss_{0:s}'.format(THESE_WINDOW_SIZE_STRINGS[m])
     this_function = custom_losses_gridded.fractions_skill_score(
-        half_window_size_px=THESE_HALF_WINDOW_SIZES_PX[m],
-        use_as_loss_function=False, function_name=this_function_name
-    )
-
-    METRIC_FUNCTION_LIST_GRIDDED.append(this_function)
-    METRIC_FUNCTION_DICT_GRIDDED[this_function_name] = this_function
-
-    this_function_name = 'gerrity_{0:s}'.format(THESE_WINDOW_SIZE_STRINGS[m])
-    this_function = custom_losses_gridded.gerrity_score(
-        half_window_size_px=THESE_HALF_WINDOW_SIZES_PX[m],
-        use_as_loss_function=False, function_name=this_function_name
-    )
-
-    METRIC_FUNCTION_LIST_GRIDDED.append(this_function)
-    METRIC_FUNCTION_DICT_GRIDDED[this_function_name] = this_function
-
-    this_function_name = 'heidke_{0:s}'.format(THESE_WINDOW_SIZE_STRINGS[m])
-    this_function = custom_losses_gridded.heidke_score(
         half_window_size_px=THESE_HALF_WINDOW_SIZES_PX[m],
         use_as_loss_function=False, function_name=this_function_name
     )
@@ -115,24 +97,6 @@ for m in range(len(THESE_HALF_WINDOW_SIZES_PX)):
 
     this_function_name = 'csi_{0:s}'.format(THESE_WINDOW_SIZE_STRINGS[m])
     this_function = custom_losses_gridded.csi(
-        half_window_size_px=THESE_HALF_WINDOW_SIZES_PX[m],
-        use_as_loss_function=False, function_name=this_function_name
-    )
-
-    METRIC_FUNCTION_LIST_GRIDDED.append(this_function)
-    METRIC_FUNCTION_DICT_GRIDDED[this_function_name] = this_function
-
-    this_function_name = 'iou_{0:s}'.format(THESE_WINDOW_SIZE_STRINGS[m])
-    this_function = custom_losses_gridded.iou(
-        half_window_size_px=THESE_HALF_WINDOW_SIZES_PX[m],
-        use_as_loss_function=False, function_name=this_function_name
-    )
-
-    METRIC_FUNCTION_LIST_GRIDDED.append(this_function)
-    METRIC_FUNCTION_DICT_GRIDDED[this_function_name] = this_function
-
-    this_function_name = 'dice_{0:s}'.format(THESE_WINDOW_SIZE_STRINGS[m])
-    this_function = custom_losses_gridded.dice_coeff(
         half_window_size_px=THESE_HALF_WINDOW_SIZES_PX[m],
         use_as_loss_function=False, function_name=this_function_name
     )
