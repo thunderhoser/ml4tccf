d2b2374beca7a39969535057e433d1e08aed2ea6
thunderhoser <lagerqui@ualberta.ca>
2023-03-09 15:06:38 -0700

    Added more loss functions and metrics for semantic segmentation.

--- ml4tccf/machine_learning/custom_losses_gridded.py
+++ ml4tccf/machine_learning/custom_losses_gridded.py
@@ -1,8 +1,24 @@
 """Custom loss functions for gridded predictions."""
 
+import tensorflow
 from tensorflow.keras import backend as K
 from gewittergefahr.gg_utils import error_checking
 from ml4tccf.utils import image_filtering
+from ml4tccf.machine_learning import custom_metrics_gridded
+
+
+def _log2(input_tensor):
+    """Computes logarithm in base 2.
+
+    :param input_tensor: Keras tensor.
+    :return: logarithm_tensor: Keras tensor with the same shape as
+        `input_tensor`.
+    """
+
+    return (
+        K.log(K.maximum(input_tensor, 1e-6)) /
+        K.log(tensorflow.Variable(2., dtype=tensorflow.float64))
+    )
 
 
 def fractions_skill_score(
@@ -51,12 +67,14 @@ def fractions_skill_score(
         """
 
         smoothed_target_tensor = K.conv2d(
-            x=target_tensor, kernel=weight_matrix,
+            x=K.expand_dims(target_tensor, axis=-1),
+            kernel=weight_matrix,
             padding='same', strides=(1, 1), data_format='channels_last'
         )
 
         smoothed_prediction_tensor = K.conv2d(
-            x=K.mean(prediction_tensor, axis=-1), kernel=weight_matrix,
+            x=K.mean(prediction_tensor, axis=-1, keepdims=True),
+            kernel=weight_matrix,
             padding='same', strides=(1, 1), data_format='channels_last'
         )
 
@@ -79,10 +97,11 @@ def fractions_skill_score(
     return loss
 
 
-def heidke_score(use_as_loss_function, function_name=None):
+def heidke_score(half_window_size_px, use_as_loss_function, function_name=None):
     """Creates Heidke-score loss function.
 
-    :param use_as_loss_function: See doc for `fractions_skill_score`.
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
     :param function_name: Same.
     :return: heidke_function: Function (defined below).
     """
@@ -97,17 +116,26 @@ def heidke_score(use_as_loss_function, function_name=None):
         :return: heidke_value: Heidke score (scalar).
         """
 
-        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+        filtered_prediction_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.mean(prediction_tensor, axis=-1, keepdims=True),
+            half_window_size_px=half_window_size_px
+        )
 
-        num_true_positives = K.sum(target_tensor * mean_prediction_tensor)
+        num_true_positives = K.sum(
+            filtered_target_tensor * filtered_prediction_tensor
+        )
         num_false_positives = K.sum(
-            (1 - target_tensor) * mean_prediction_tensor
+            (1 - filtered_target_tensor) * filtered_prediction_tensor
         )
         num_false_negatives = K.sum(
-            target_tensor * (1 - mean_prediction_tensor)
+            filtered_target_tensor * (1 - filtered_prediction_tensor)
         )
         num_true_negatives = K.sum(
-            (1 - target_tensor) * (1 - mean_prediction_tensor)
+            (1 - filtered_target_tensor) * (1 - filtered_prediction_tensor)
         )
 
         random_num_correct = (
@@ -136,10 +164,11 @@ def heidke_score(use_as_loss_function, function_name=None):
     return heidke_function
 
 
-def peirce_score(use_as_loss_function, function_name=None):
+def peirce_score(half_window_size_px, use_as_loss_function, function_name=None):
     """Creates Peirce-score loss function.
 
-    :param use_as_loss_function: See doc for `fractions_skill_score`.
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
     :param function_name: Same.
     :return: peirce_function: Function (defined below).
     """
@@ -154,17 +183,26 @@ def peirce_score(use_as_loss_function, function_name=None):
         :return: peirce_value: Peirce score (scalar).
         """
 
-        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+        filtered_prediction_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.mean(prediction_tensor, axis=-1, keepdims=True),
+            half_window_size_px=half_window_size_px
+        )
 
-        num_true_positives = K.sum(target_tensor * mean_prediction_tensor)
+        num_true_positives = K.sum(
+            filtered_target_tensor * filtered_prediction_tensor
+        )
         num_false_positives = K.sum(
-            (1 - target_tensor) * mean_prediction_tensor
+            (1 - filtered_target_tensor) * filtered_prediction_tensor
         )
         num_false_negatives = K.sum(
-            target_tensor * (1 - mean_prediction_tensor)
+            filtered_target_tensor * (1 - filtered_prediction_tensor)
         )
         num_true_negatives = K.sum(
-            (1 - target_tensor) * (1 - mean_prediction_tensor)
+            (1 - filtered_target_tensor) * (1 - filtered_prediction_tensor)
         )
 
         pod_value = (
@@ -187,10 +225,12 @@ def peirce_score(use_as_loss_function, function_name=None):
     return peirce_function
 
 
-def gerrity_score(use_as_loss_function, function_name=None):
+def gerrity_score(half_window_size_px, use_as_loss_function,
+                  function_name=None):
     """Creates Gerrity-score loss function.
 
-    :param use_as_loss_function: See doc for `fractions_skill_score`.
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
     :param function_name: Same.
     :return: gerrity_function: Function (defined below).
     """
@@ -205,17 +245,26 @@ def gerrity_score(use_as_loss_function, function_name=None):
         :return: gerrity_value: Gerrity score (scalar).
         """
 
-        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+        filtered_prediction_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.mean(prediction_tensor, axis=-1, keepdims=True),
+            half_window_size_px=half_window_size_px
+        )
 
-        num_true_positives = K.sum(target_tensor * mean_prediction_tensor)
+        num_true_positives = K.sum(
+            filtered_target_tensor * filtered_prediction_tensor
+        )
         num_false_positives = K.sum(
-            (1 - target_tensor) * mean_prediction_tensor
+            (1 - filtered_target_tensor) * filtered_prediction_tensor
         )
         num_false_negatives = K.sum(
-            target_tensor * (1 - mean_prediction_tensor)
+            filtered_target_tensor * (1 - filtered_prediction_tensor)
         )
         num_true_negatives = K.sum(
-            (1 - target_tensor) * (1 - mean_prediction_tensor)
+            (1 - filtered_target_tensor) * (1 - filtered_prediction_tensor)
         )
 
         event_ratio = (
@@ -242,3 +291,295 @@ def gerrity_score(use_as_loss_function, function_name=None):
         gerrity_function.__name__ = function_name
 
     return gerrity_function
+
+
+def csi(half_window_size_px, use_as_loss_function, function_name=None):
+    """Creates critical success index (CSI) loss function.
+
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
+    :param function_name: Same.
+    :return: csi_function: Function (defined below).
+    """
+
+    error_checking.assert_is_boolean(use_as_loss_function)
+
+    def csi_function(target_tensor, prediction_tensor):
+        """Computes CSI.
+
+        :param target_tensor: See doc for `fractions_skill_score`.
+        :param prediction_tensor: Same.
+        :return: csi_value: CSI (scalar).
+        """
+
+        pod_value = K.epsilon() + custom_metrics_gridded.pod(
+            half_window_size_px=half_window_size_px
+        )(target_tensor=target_tensor, prediction_tensor=prediction_tensor)
+
+        success_ratio_value = K.epsilon() + custom_metrics_gridded.success_ratio(
+            half_window_size_px=half_window_size_px
+        )(target_tensor=target_tensor, prediction_tensor=prediction_tensor)
+
+        csi_value = (pod_value ** -1 + success_ratio_value ** -1 - 1) ** -1
+
+        if use_as_loss_function:
+            return 1. - csi_value
+
+        return csi_value
+
+    if function_name is not None:
+        csi_function.__name__ = function_name
+
+    return csi_function
+
+
+def iou(half_window_size_px, use_as_loss_function, function_name=None):
+    """Creates intersection over union (IOU) loss function.
+
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
+    :param function_name: Same.
+    :return: iou_function: Function (defined below).
+    """
+
+    error_checking.assert_is_boolean(use_as_loss_function)
+
+    def iou_function(target_tensor, prediction_tensor):
+        """Computes IOU.
+
+        :param target_tensor: See doc for `fractions_skill_score`.
+        :param prediction_tensor: Same.
+        :return: iou_value: IOU (scalar).
+        """
+
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+
+        filtered_target_tensor = filtered_target_tensor[..., 0]
+        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+
+        intersection_tensor = K.sum(
+            filtered_target_tensor * mean_prediction_tensor, axis=(1, 2)
+        )
+        union_tensor = (
+            K.sum(filtered_target_tensor, axis=(1, 2)) +
+            K.sum(mean_prediction_tensor, axis=(1, 2)) -
+            intersection_tensor
+        )
+
+        iou_value = K.mean(
+            intersection_tensor / (union_tensor + K.epsilon())
+        )
+
+        if use_as_loss_function:
+            return 1. - iou_value
+
+        return iou_value
+
+    if function_name is not None:
+        iou_function.__name__ = function_name
+
+    return iou_function
+
+
+def all_class_iou(half_window_size_px, use_as_loss_function,
+                  function_name=None):
+    """Creates all-class intersection over union (IOU) loss function.
+
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
+    :param function_name: Same.
+    :return: all_class_iou_function: Function (defined below).
+    """
+
+    error_checking.assert_is_boolean(use_as_loss_function)
+
+    def all_class_iou_function(target_tensor, prediction_tensor):
+        """Computes IOU.
+
+        :param target_tensor: See doc for `fractions_skill_score`.
+        :param prediction_tensor: Same.
+        :return: all_class_iou_value: All-class IOU (scalar).
+        """
+
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+
+        filtered_target_tensor = filtered_target_tensor[..., 0]
+        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+
+        positive_intersection_tensor = K.sum(
+            filtered_target_tensor * mean_prediction_tensor, axis=(1, 2)
+        )
+        positive_union_tensor = (
+            K.sum(filtered_target_tensor, axis=(1, 2)) +
+            K.sum(mean_prediction_tensor, axis=(1, 2)) -
+            positive_intersection_tensor
+        )
+
+        negative_intersection_tensor = K.sum(
+            (1. - filtered_target_tensor) * (1. - mean_prediction_tensor),
+            axis=(1, 2)
+        )
+        negative_union_tensor = (
+            K.sum(1. - filtered_target_tensor, axis=(1, 2)) +
+            K.sum(1. - mean_prediction_tensor, axis=(1, 2)) -
+            negative_intersection_tensor
+        )
+
+        positive_iou = K.mean(
+            positive_intersection_tensor / (positive_union_tensor + K.epsilon())
+        )
+        negative_iou = K.mean(
+            negative_intersection_tensor / (negative_union_tensor + K.epsilon())
+        )
+        iou_value = (positive_iou + negative_iou) / 2
+
+        if use_as_loss_function:
+            return 1. - iou_value
+
+        return iou_value
+
+    if function_name is not None:
+        all_class_iou_function.__name__ = function_name
+
+    return all_class_iou_function
+
+
+def dice_coeff(half_window_size_px, use_as_loss_function, function_name=None):
+    """Creates Dice-coefficient loss function.
+
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param use_as_loss_function: Same.
+    :param function_name: Same.
+    :return: dice_coeff_function: Function (defined below).
+    """
+
+    error_checking.assert_is_boolean(use_as_loss_function)
+
+    def dice_coeff_function(target_tensor, prediction_tensor):
+        """Computes Dice coefficient.
+
+        :param target_tensor: See doc for `fractions_skill_score`.
+        :param prediction_tensor: Same.
+        :return: dice_coeff_value: Dice coefficient (scalar).
+        """
+
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+
+        filtered_target_tensor = filtered_target_tensor[..., 0]
+        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+
+        positive_intersection_tensor = K.sum(
+            filtered_target_tensor * mean_prediction_tensor, axis=(1, 2)
+        )
+        negative_intersection_tensor = K.sum(
+            (1. - filtered_target_tensor) * (1. - mean_prediction_tensor),
+            axis=(1, 2)
+        )
+        num_pixels_tensor = K.sum(
+            K.ones_like(mean_prediction_tensor), axis=(1, 2)
+        )
+
+        dice_value = K.mean(
+            (positive_intersection_tensor + negative_intersection_tensor) /
+            num_pixels_tensor
+        )
+
+        if use_as_loss_function:
+            return 1. - dice_value
+
+        return dice_value
+
+    if function_name is not None:
+        dice_coeff_function.__name__ = function_name
+
+    return dice_coeff_function
+
+
+def brier_score(half_window_size_px, function_name=None):
+    """Creates Brier-score loss function.
+
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param function_name: Same.
+    :return: brier_function: Function (defined below).
+    """
+
+    def brier_function(target_tensor, prediction_tensor):
+        """Computes Brier score.
+
+        :param target_tensor: See doc for `fractions_skill_score`.
+        :param prediction_tensor: Same.
+        :return: brier_value: Brier score (scalar).
+        """
+
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+
+        filtered_target_tensor = filtered_target_tensor[..., 0]
+        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+
+        squared_error_tensor = K.sum(
+            (filtered_target_tensor - mean_prediction_tensor) ** 2,
+            axis=(1, 2)
+        )
+        num_pixels_tensor = K.sum(
+            K.ones_like(mean_prediction_tensor), axis=(1, 2)
+        )
+
+        return K.mean(squared_error_tensor / num_pixels_tensor)
+
+    if function_name is not None:
+        brier_function.__name__ = function_name
+
+    return brier_function
+
+
+def cross_entropy(half_window_size_px, function_name=None):
+    """Creates cross-entropy loss function.
+
+    :param half_window_size_px: See doc for `fractions_skill_score`.
+    :param function_name: Same.
+    :return: xentropy_function: Function (defined below).
+    """
+
+    def xentropy_function(target_tensor, prediction_tensor):
+        """Computes cross-entropy.
+
+        :param target_tensor: See doc for `fractions_skill_score`.
+        :param prediction_tensor: Same.
+        :return: xentropy_value: Cross-entropy (scalar).
+        """
+
+        filtered_target_tensor = custom_metrics_gridded.apply_max_filter(
+            input_tensor=K.expand_dims(target_tensor, axis=-1),
+            half_window_size_px=half_window_size_px
+        )
+
+        filtered_target_tensor = filtered_target_tensor[..., 0]
+        mean_prediction_tensor = K.mean(prediction_tensor, axis=-1)
+
+        xentropy_tensor = K.sum(
+            filtered_target_tensor * _log2(mean_prediction_tensor) +
+            (1. - filtered_target_tensor) * _log2(1. - mean_prediction_tensor),
+            axis=(1, 2)
+        )
+        num_pixels_tensor = K.sum(
+            K.ones_like(mean_prediction_tensor), axis=(1, 2)
+        )
+
+        return -K.mean(xentropy_tensor / num_pixels_tensor)
+
+    if function_name is not None:
+        xentropy_function.__name__ = function_name
+
+    return xentropy_function
