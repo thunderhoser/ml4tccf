89424df5121bf030407be3a1e91f29ffd4c3f1e8
thunderhoser <lagerqui@ualberta.ca>
2023-03-20 17:27:47 -0600

    FOO

--- ml4tccf/satellite_utils.py
+++ ml4tccf/satellite_utils.py
@@ -749,6 +749,11 @@ def subset_times(satellite_table_xarray, desired_times_unix_sec,
             except:
                 print('INTERP FOOOOOOOOOOOoo')
 
+            print(fill_value_tuple)
+            print(numpy.max(numpy.diff(source_table_xarray.coords[TIME_DIM].values)))
+            print(numpy.min(numpy.diff(source_table_xarray.coords[TIME_DIM].values)))
+            print(numpy.diff(source_table_xarray.coords[TIME_DIM].values))
+
             interp_object = interp1d(
                 x=source_table_xarray.coords[TIME_DIM].values,
                 y=source_table_xarray[BRIGHTNESS_TEMPERATURE_KEY].values[
