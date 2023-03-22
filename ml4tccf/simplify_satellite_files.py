0d1de87ecea55e73316e6beee883700181ae4224
thunderhoser <lagerqui@ualberta.ca>
2023-03-22 14:42:50 -0600

    FOO

--- ml4tccf/simplify_satellite_files.py
+++ ml4tccf/simplify_satellite_files.py
@@ -102,6 +102,10 @@ def _run(input_dir_name, cyclone_id_string, output_dir_name):
                 numpy.sum(bad_time_flags), len(bad_time_flags)
             ))
 
+        if len(good_time_indices) == 0:
+            del satellite_table_xarray
+            continue
+
         satellite_table_xarray = satellite_table_xarray.isel(
             indexers={satellite_utils.TIME_DIM: good_time_indices}
         )
