241d767cefa766e96ea244f8b5e1d93f2d8a14a1
thunderhoser <lagerqui@ualberta.ca>
2023-04-06 20:56:54 -0600

    FOO

---
+++ ml4tccf/cira_ir_satellite_io.py
@@ -0,0 +1,156 @@
+"""IO methods for processed satellite data."""
+
+import os
+import sys
+import glob
+import xarray
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import file_system_utils
+import error_checking
+import cira_ir_satellite_utils
+
+GZIP_FILE_EXTENSION = '.gz'
+CYCLONE_ID_REGEX = '[0-9][0-9][0-9][0-9][A-Z][A-Z][0-9][0-9]'
+
+
+def find_file(directory_name, cyclone_id_string, prefer_zipped=True,
+              allow_other_format=True, raise_error_if_missing=True):
+    """Finds NetCDF file with satellite data.
+
+    :param directory_name: Name of directory with satellite data.
+    :param cyclone_id_string: Cyclone ID (must be accepted by
+        `cira_ir_satellite_utils.parse_cyclone_id`).
+    :param prefer_zipped: Boolean flag.  If True, will look for zipped file
+        first.  If False, will look for unzipped file first.
+    :param allow_other_format: Boolean flag.  If True, will allow opposite of
+        preferred file format (zipped or unzipped).
+    :param raise_error_if_missing: Boolean flag.  If file is missing and
+        `raise_error_if_missing == True`, will throw error.  If file is missing
+        and `raise_error_if_missing == False`, will return *expected* file path.
+    :return: satellite_file_name: File path.
+    :raises: ValueError: if file is missing
+        and `raise_error_if_missing == True`.
+    """
+
+    error_checking.assert_is_string(directory_name)
+    cira_ir_satellite_utils.parse_cyclone_id(cyclone_id_string)
+    error_checking.assert_is_boolean(prefer_zipped)
+    error_checking.assert_is_boolean(allow_other_format)
+    error_checking.assert_is_boolean(raise_error_if_missing)
+
+    satellite_file_name = '{0:s}/cira_satellite_{1:s}.nc{2:s}'.format(
+        directory_name, cyclone_id_string,
+        GZIP_FILE_EXTENSION if prefer_zipped else ''
+    )
+
+    if os.path.isfile(satellite_file_name):
+        return satellite_file_name
+
+    if allow_other_format:
+        if prefer_zipped:
+            satellite_file_name = (
+                satellite_file_name[:-len(GZIP_FILE_EXTENSION)]
+            )
+        else:
+            satellite_file_name += GZIP_FILE_EXTENSION
+
+    if os.path.isfile(satellite_file_name) or not raise_error_if_missing:
+        return satellite_file_name
+
+    error_string = 'Cannot find file.  Expected at: "{0:s}"'.format(
+        satellite_file_name
+    )
+    raise ValueError(error_string)
+
+
+def find_cyclones(directory_name, raise_error_if_all_missing=True):
+    """Finds all cyclones.
+
+    :param directory_name: Name of directory with satellite data.
+    :param raise_error_if_all_missing: Boolean flag.  If no cyclones are found
+        and `raise_error_if_all_missing == True`, will throw error.  If no
+        cyclones are found and `raise_error_if_all_missing == False`, will
+        return empty list.
+    :return: cyclone_id_strings: List of cyclone IDs.
+    :raises: ValueError: if file is missing
+        and `raise_error_if_missing == True`.
+    """
+
+    error_checking.assert_is_string(directory_name)
+    error_checking.assert_is_boolean(raise_error_if_all_missing)
+
+    file_pattern = '{0:s}/cira_satellite_{1:s}.nc'.format(
+        directory_name, CYCLONE_ID_REGEX
+    )
+    satellite_file_names = glob.glob(file_pattern)
+    file_pattern = '{0:s}{1:s}'.format(file_pattern, GZIP_FILE_EXTENSION)
+    satellite_file_names += glob.glob(file_pattern)
+
+    cyclone_id_strings = []
+
+    for this_file_name in satellite_file_names:
+        try:
+            cyclone_id_strings.append(
+                file_name_to_cyclone_id(this_file_name)
+            )
+        except:
+            pass
+
+    cyclone_id_strings = list(set(cyclone_id_strings))
+    cyclone_id_strings.sort()
+
+    if raise_error_if_all_missing and len(cyclone_id_strings) == 0:
+        error_string = (
+            'Could not find any cyclone IDs from files with pattern: "{0:s}"'
+        ).format(file_pattern)
+
+        raise ValueError(error_string)
+
+    return cyclone_id_strings
+
+
+def file_name_to_cyclone_id(satellite_file_name):
+    """Parses cyclone ID from name of file with satellite data.
+
+    :param satellite_file_name: File path.
+    :return: cyclone_id_string: Cyclone ID.
+    """
+
+    error_checking.assert_is_string(satellite_file_name)
+    pathless_file_name = os.path.split(satellite_file_name)[1]
+
+    cyclone_id_string = pathless_file_name.split('.')[0].split('_')[-1]
+    cira_ir_satellite_utils.parse_cyclone_id(cyclone_id_string)
+
+    return cyclone_id_string
+
+
+def read_file(netcdf_file_name):
+    """Reads satellite data from NetCDF file.
+
+    :param netcdf_file_name: Path to input file.
+    :return: satellite_table_xarray: xarray table.  Documentation in the xarray
+        table should make values self-explanatory.
+    """
+
+    return xarray.open_dataset(netcdf_file_name)
+
+
+def write_file(satellite_table_xarray, netcdf_file_name):
+    """Writes satellite data to NetCDF file.
+
+    :param satellite_table_xarray: xarray table in format returned by
+        `read_file`.
+    :param netcdf_file_name: Path to output file.
+    """
+
+    file_system_utils.mkdir_recursive_if_necessary(file_name=netcdf_file_name)
+
+    satellite_table_xarray.to_netcdf(
+        path=netcdf_file_name, mode='w', format='NETCDF3_64BIT'
+    )
---
+++ ml4tccf/cira_ir_satellite_utils.py
@@ -0,0 +1,141 @@
+"""Helper methods for satellite data."""
+
+import os
+import sys
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import error_checking
+
+GRID_SPACING_METRES = 4000.
+DEFAULT_NUM_GRID_ROWS = 480
+DEFAULT_NUM_GRID_COLUMNS = 640
+
+GRID_ROW_DIM = 'satellite_grid_row'
+GRID_COLUMN_DIM = 'satellite_grid_column'
+TIME_DIM = 'satellite_valid_time_unix_sec'
+
+SATELLITE_NUMBER_KEY = 'satellite_number'
+BAND_NUMBER_KEY = 'satellite_band_number'
+BAND_WAVELENGTH_KEY = 'satellite_band_wavelength_micrometres'
+SATELLITE_LONGITUDE_KEY = 'satellite_longitude_deg_e'
+CYCLONE_ID_KEY = 'satellite_cyclone_id_string'
+STORM_TYPE_KEY = 'satellite_storm_type_string'
+STORM_NAME_KEY = 'satellite_storm_name'
+STORM_LATITUDE_KEY = 'satellite_storm_latitude_deg_n'
+STORM_LONGITUDE_KEY = 'satellite_storm_longitude_deg_e'
+STORM_INTENSITY_KEY = 'satellite_storm_intensity_m_s01'
+STORM_INTENSITY_NUM_KEY = 'satellite_storm_intensity_number'
+STORM_MOTION_U_KEY = 'satellite_storm_u_motion_m_s01'
+STORM_MOTION_V_KEY = 'satellite_storm_v_motion_m_s01'
+STORM_DISTANCE_TO_LAND_KEY = 'satellite_storm_distance_to_land_metres'
+STORM_RADIUS_VERSION1_KEY = 'satellite_storm_radius_version1_metres'
+STORM_RADIUS_VERSION2_KEY = 'satellite_storm_radius_version2_metres'
+STORM_RADIUS_FRACTIONAL_KEY = 'satellite_storm_radius_fractional'
+SATELLITE_AZIMUTH_ANGLE_SIN_KEY = 'satellite_azimuth_angle_sin'
+SATELLITE_AZIMUTH_ANGLE_COS_KEY = 'satellite_azimuth_angle_cos'
+SATELLITE_ZENITH_ANGLE_KEY = 'satellite_zenith_angle_deg'
+SOLAR_AZIMUTH_ANGLE_SIN_KEY = 'satellite_solar_azimuth_angle_sin'
+SOLAR_AZIMUTH_ANGLE_COS_KEY = 'satellite_solar_azimuth_angle_cos'
+SOLAR_ZENITH_ANGLE_KEY = 'satellite_solar_zenith_angle_deg'
+SOLAR_ELEVATION_ANGLE_KEY = 'satellite_solar_elevation_angle_deg'
+SOLAR_HOUR_ANGLE_SIN_KEY = 'satellite_solar_hour_angle_sin'
+SOLAR_HOUR_ANGLE_COS_KEY = 'satellite_solar_hour_angle_cos'
+BRIGHTNESS_TEMPERATURE_KEY = 'satellite_brightness_temp_kelvins'
+GRID_LATITUDE_KEY = 'satellite_grid_latitude_deg_n'
+GRID_LONGITUDE_KEY = 'satellite_grid_longitude_deg_e'
+
+FIELD_NAMES = [
+    SATELLITE_NUMBER_KEY, BAND_NUMBER_KEY, BAND_WAVELENGTH_KEY,
+    SATELLITE_LONGITUDE_KEY, CYCLONE_ID_KEY, STORM_TYPE_KEY,
+    STORM_NAME_KEY, STORM_LATITUDE_KEY, STORM_LONGITUDE_KEY,
+    STORM_INTENSITY_KEY, STORM_INTENSITY_NUM_KEY, STORM_MOTION_U_KEY,
+    STORM_MOTION_V_KEY, STORM_DISTANCE_TO_LAND_KEY, STORM_RADIUS_VERSION1_KEY,
+    STORM_RADIUS_VERSION2_KEY, STORM_RADIUS_FRACTIONAL_KEY,
+    SATELLITE_AZIMUTH_ANGLE_SIN_KEY, SATELLITE_AZIMUTH_ANGLE_COS_KEY,
+    SATELLITE_ZENITH_ANGLE_KEY,
+    SOLAR_AZIMUTH_ANGLE_SIN_KEY, SOLAR_AZIMUTH_ANGLE_COS_KEY,
+    SOLAR_ZENITH_ANGLE_KEY, SOLAR_ELEVATION_ANGLE_KEY,
+    SOLAR_HOUR_ANGLE_SIN_KEY, SOLAR_HOUR_ANGLE_COS_KEY,
+    BRIGHTNESS_TEMPERATURE_KEY, GRID_LATITUDE_KEY, GRID_LONGITUDE_KEY
+]
+
+NORTH_ATLANTIC_ID_STRING = 'AL'
+SOUTH_ATLANTIC_ID_STRING = 'SL'
+NORTHEAST_PACIFIC_ID_STRING = 'EP'
+NORTH_CENTRAL_PACIFIC_ID_STRING = 'CP'
+NORTHWEST_PACIFIC_ID_STRING = 'WP'
+NORTH_INDIAN_ID_STRING = 'IO'
+SOUTHERN_HEMISPHERE_ID_STRING = 'SH'
+
+VALID_BASIN_ID_STRINGS = [
+    NORTH_ATLANTIC_ID_STRING, SOUTH_ATLANTIC_ID_STRING,
+    NORTHEAST_PACIFIC_ID_STRING, NORTH_CENTRAL_PACIFIC_ID_STRING,
+    NORTHWEST_PACIFIC_ID_STRING, NORTH_INDIAN_ID_STRING,
+    SOUTHERN_HEMISPHERE_ID_STRING
+]
+
+
+def check_basin_id(basin_id_string):
+    """Ensures that basin ID is valid.
+
+    :param basin_id_string: Basin ID.
+    :raises: ValueError: if `basin_id_strings not in VALID_BASIN_ID_STRINGS`.
+    """
+
+    error_checking.assert_is_string(basin_id_string)
+    if basin_id_string in VALID_BASIN_ID_STRINGS:
+        return
+
+    error_string = (
+        'Basin ID ("{0:s}") must be in the following list:\n{1:s}'
+    ).format(basin_id_string, str(VALID_BASIN_ID_STRINGS))
+
+    raise ValueError(error_string)
+
+
+def get_cyclone_id(year, basin_id_string, cyclone_number):
+    """Creates cyclone ID from metadata.
+
+    :param year: Year (integer).
+    :param basin_id_string: Basin ID (must be accepted by `check_basin_id`).
+    :param cyclone_number: Cyclone number (integer).
+    :return: cyclone_id_string: Cyclone ID.
+    """
+
+    error_checking.assert_is_integer(year)
+    error_checking.assert_is_geq(year, 0)
+    check_basin_id(basin_id_string)
+    error_checking.assert_is_integer(cyclone_number)
+    error_checking.assert_is_greater(cyclone_number, 0)
+
+    return '{0:04d}{1:s}{2:02d}'.format(year, basin_id_string, cyclone_number)
+
+
+def parse_cyclone_id(cyclone_id_string):
+    """Parses metadata from cyclone ID.
+
+    :param cyclone_id_string: Cyclone ID, formatted like "yyyybbcc", where yyyy
+        is the year; bb is the basin ID; and cc is the cyclone number ([cc]th
+        cyclone of the season in the given basin).
+    :return: year: Year (integer).
+    :return: basin_id_string: Basin ID.
+    :return: cyclone_number: Cyclone number (integer).
+    """
+
+    error_checking.assert_is_string(cyclone_id_string)
+    assert len(cyclone_id_string) == 8
+
+    year = int(cyclone_id_string[:4])
+    error_checking.assert_is_geq(year, 0)
+
+    basin_id_string = cyclone_id_string[4:6]
+    check_basin_id(basin_id_string)
+
+    cyclone_number = int(cyclone_id_string[6:])
+    error_checking.assert_is_greater(cyclone_number, 0)
+
+    return year, basin_id_string, cyclone_number
