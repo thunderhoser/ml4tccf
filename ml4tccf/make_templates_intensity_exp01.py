ec0558480459182456dce0f0d6080f4307407c34
thunderhoser <lagerqui@ualberta.ca>
2023-08-11 13:10:42 -0600

    FOO

--- ml4tccf/make_templates_intensity_exp01.py
+++ ml4tccf/make_templates_intensity_exp01.py
@@ -20,7 +20,7 @@ import accum_grad_optimizer
 
 OUTPUT_DIR_NAME = (
     '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_models/'
-    'intensity_exp01/templates'
+    'intensity_experiment01/templates'
 )
 
 NUM_SCALAR_PREDICTORS = 5
@@ -70,103 +70,102 @@ def _run():
     """
 
     for i in range(len(FIRST_LAYER_FILTER_COUNTS)):
-        for k in range(len(BATCHES_PER_UPDATE_COUNTS)):
-            option_dict = copy.deepcopy(DEFAULT_OPTION_DICT)
+        option_dict = copy.deepcopy(DEFAULT_OPTION_DICT)
 
-            num_channels_by_conv_layer = numpy.linspace(
-                1, NUM_CONV_BLOCKS, num=NUM_CONV_BLOCKS, dtype=int
-            ) * FIRST_LAYER_FILTER_COUNTS[i]
+        num_channels_by_conv_layer = numpy.linspace(
+            1, NUM_CONV_BLOCKS, num=NUM_CONV_BLOCKS, dtype=int
+        ) * FIRST_LAYER_FILTER_COUNTS[i]
 
-            (
-                dense_neuron_counts
-            ) = architecture_utils.get_dense_layer_dimensions(
-                num_input_units=
-                NUM_SCALAR_PREDICTORS + 28 * num_channels_by_conv_layer[-1],
-                num_classes=2,
-                num_dense_layers=NUM_DENSE_LAYERS,
-                for_classification=True
-            )[1]
+        (
+            dense_neuron_counts
+        ) = architecture_utils.get_dense_layer_dimensions(
+            num_input_units=
+            NUM_SCALAR_PREDICTORS + 28 * num_channels_by_conv_layer[-1],
+            num_classes=2,
+            num_dense_layers=NUM_DENSE_LAYERS,
+            for_classification=True
+        )[1]
 
-            if BATCHES_PER_UPDATE_COUNTS[k] > 1:
-                optimizer_function = (
-                    accum_grad_optimizer.convert_to_accumulate_gradient_optimizer(
-                        orig_optimizer=keras.optimizers.Adam(),
-                        update_params_frequency=BATCHES_PER_UPDATE_COUNTS[k],
-                        accumulate_sum_or_mean=False
-                    )
+        if BATCHES_PER_UPDATE_COUNTS[i] > 1:
+            optimizer_function = (
+                accum_grad_optimizer.convert_to_accumulate_gradient_optimizer(
+                    orig_optimizer=keras.optimizers.Adam(),
+                    update_params_frequency=BATCHES_PER_UPDATE_COUNTS[i],
+                    accumulate_sum_or_mean=False
                 )
+            )
 
-                optimizer_string = (
-                    'accum_grad_optimizer.convert_to_accumulate_gradient_optimizer('
-                    'orig_optimizer=keras.optimizers.Adam(), '
-                    'update_params_frequency={0:d}, '
-                    'accumulate_sum_or_mean=False)'.format(
-                        BATCHES_PER_UPDATE_COUNTS[k]
-                    )
+            optimizer_string = (
+                'accum_grad_optimizer.convert_to_accumulate_gradient_optimizer('
+                'orig_optimizer=keras.optimizers.Adam(), '
+                'update_params_frequency={0:d}, '
+                'accumulate_sum_or_mean=False)'.format(
+                    BATCHES_PER_UPDATE_COUNTS[i]
                 )
-            else:
-                optimizer_function = keras.optimizers.Adam()
-                optimizer_string = 'keras.optimizers.Adam()'
-
-            option_dict.update({
-                cnn_architecture.NUM_CHANNELS_KEY: num_channels_by_conv_layer,
-                cnn_architecture.NUM_NEURONS_KEY: dense_neuron_counts,
-                cnn_architecture.OPTIMIZER_FUNCTION_KEY: optimizer_function
-            })
-            model_object = cnn_architecture.create_intensity_model(option_dict)
-
-            output_file_name = (
-                '{0:s}/num-first-layer-filters={1:02d}_'
-                'num-batches-per-update={2:d}/model.h5'
-            ).format(
-                OUTPUT_DIR_NAME,
-                FIRST_LAYER_FILTER_COUNTS[i],
-                BATCHES_PER_UPDATE_COUNTS[k]
             )
+        else:
+            optimizer_function = keras.optimizers.Adam()
+            optimizer_string = 'keras.optimizers.Adam()'
 
-            file_system_utils.mkdir_recursive_if_necessary(
-                file_name=output_file_name
-            )
+        option_dict.update({
+            cnn_architecture.NUM_CHANNELS_KEY: num_channels_by_conv_layer,
+            cnn_architecture.NUM_NEURONS_KEY: dense_neuron_counts,
+            cnn_architecture.OPTIMIZER_FUNCTION_KEY: optimizer_function
+        })
+        model_object = cnn_architecture.create_intensity_model(option_dict)
 
-            print('Writing model to: "{0:s}"...'.format(output_file_name))
-            model_object.save(
-                filepath=output_file_name, overwrite=True,
-                include_optimizer=True
-            )
+        output_file_name = (
+            '{0:s}/num-first-layer-filters={1:02d}_'
+            'num-batches-per-update={2:d}/model.h5'
+        ).format(
+            OUTPUT_DIR_NAME,
+            FIRST_LAYER_FILTER_COUNTS[i],
+            BATCHES_PER_UPDATE_COUNTS[i]
+        )
 
-            metafile_name = neural_net_utils.find_metafile(
-                model_dir_name=os.path.split(output_file_name)[0],
-                raise_error_if_missing=False
-            )
+        file_system_utils.mkdir_recursive_if_necessary(
+            file_name=output_file_name
+        )
 
-            option_dict[cnn_architecture.LOSS_FUNCTION_KEY] = (
-                LOSS_FUNCTION_STRING
-            )
-            option_dict[cnn_architecture.OPTIMIZER_FUNCTION_KEY] = (
-                optimizer_string
-            )
+        print('Writing model to: "{0:s}"...'.format(output_file_name))
+        model_object.save(
+            filepath=output_file_name, overwrite=True,
+            include_optimizer=True
+        )
 
-            neural_net_utils.write_metafile(
-                pickle_file_name=metafile_name,
-                num_epochs=100,
-                num_training_batches_per_epoch=32,
-                training_option_dict={
-                    neural_net_utils.SEMANTIC_SEG_FLAG_KEY: False
-                },
-                num_validation_batches_per_epoch=16,
-                validation_option_dict={
-                    neural_net_utils.SEMANTIC_SEG_FLAG_KEY: False
-                },
-                loss_function_string=LOSS_FUNCTION_STRING,
-                optimizer_function_string=optimizer_string,
-                plateau_patience_epochs=10,
-                plateau_learning_rate_multiplier=0.6,
-                early_stopping_patience_epochs=50,
-                architecture_dict=option_dict,
-                is_model_bnn=False,
-                data_type_string=neural_net_utils.RG_SIMPLE_DATA_TYPE_STRING,
-                train_with_shuffled_data=True
-            )
+        metafile_name = neural_net_utils.find_metafile(
+            model_dir_name=os.path.split(output_file_name)[0],
+            raise_error_if_missing=False
+        )
+
+        option_dict[cnn_architecture.LOSS_FUNCTION_KEY] = (
+            LOSS_FUNCTION_STRING
+        )
+        option_dict[cnn_architecture.OPTIMIZER_FUNCTION_KEY] = (
+            optimizer_string
+        )
+
+        neural_net_utils.write_metafile(
+            pickle_file_name=metafile_name,
+            num_epochs=100,
+            num_training_batches_per_epoch=32,
+            training_option_dict={
+                neural_net_utils.SEMANTIC_SEG_FLAG_KEY: False
+            },
+            num_validation_batches_per_epoch=16,
+            validation_option_dict={
+                neural_net_utils.SEMANTIC_SEG_FLAG_KEY: False
+            },
+            loss_function_string=LOSS_FUNCTION_STRING,
+            optimizer_function_string=optimizer_string,
+            plateau_patience_epochs=10,
+            plateau_learning_rate_multiplier=0.6,
+            early_stopping_patience_epochs=50,
+            architecture_dict=option_dict,
+            is_model_bnn=False,
+            data_type_string=neural_net_utils.RG_SIMPLE_DATA_TYPE_STRING,
+            train_with_shuffled_data=True
+        )
 
 
 if __name__ == '__main__':
