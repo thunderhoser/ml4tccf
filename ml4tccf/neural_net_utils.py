1c0856e02c6da2db058c48fa5596e89cc7274349
thunderhoser <lagerqui@ualberta.ca>
2023-11-15 14:49:10 -0700

    FOO

--- ml4tccf/neural_net_utils.py
+++ ml4tccf/neural_net_utils.py
@@ -1065,6 +1065,8 @@ def read_model(hdf5_file_name):
                         )
                     )
 
+            print(architecture_dict)
+
             if temporal_cnn_architecture.FC_MODULE_USE_3D_CONV in architecture_dict:
                 model_object = temporal_cnn_architecture.create_model(architecture_dict)
             else:
