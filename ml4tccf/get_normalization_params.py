0ecbf84deed6f8ecc14a5e8a0260e18d71751990
thunderhoser <lagerqui@ualberta.ca>
2023-11-21 14:20:00 -0700

    FOO

--- ml4tccf/get_normalization_params.py
+++ ml4tccf/get_normalization_params.py
@@ -19,6 +19,7 @@ SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
 INPUT_DIR_ARG_NAME = 'input_satellite_dir_name'
 YEARS_ARG_NAME = 'years'
+CYCLONE_IDS_ARG_NAME = 'cyclone_id_strings'
 NUM_FILES_ARG_NAME = 'num_satellite_files'
 NUM_VALUES_PER_HIGH_RES_ARG_NAME = 'num_values_per_high_res_channel'
 NUM_VALUES_PER_LOW_RES_ARG_NAME = 'num_values_per_low_res_channel'
@@ -29,7 +30,13 @@ INPUT_DIR_HELP_STRING = (
     '`satellite_io.find_file` and read by `satellite_io.read_file`.'
 )
 YEARS_HELP_STRING = (
-    'Will use data from these years (list) to compute normalization params.'
+    'Will use years in this list to compute normalization params.  If you '
+    'would rather specify cyclone IDs, leave this argument alone.'
+)
+CYCLONE_IDS_HELP_STRING = (
+    'Will use cyclones in this list (format "yyyyBBnn") to compute '
+    'normalization params.  If you would rather specify years, leave this '
+    'argument alone.'
 )
 NUM_FILES_HELP_STRING = (
     'Will randomly select this many satellite files from the years in `{0:s}`.'
@@ -52,8 +59,12 @@ INPUT_ARG_PARSER.add_argument(
     help=INPUT_DIR_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
-    '--' + YEARS_ARG_NAME, type=int, nargs='+', required=True,
+    '--' + YEARS_ARG_NAME, type=int, nargs='+', required=False, default=[-1],
     help=YEARS_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + CYCLONE_IDS_ARG_NAME, type=str, nargs='+', required=False,
+    default=[''], help=CYCLONE_IDS_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
     '--' + NUM_FILES_ARG_NAME, type=int, required=True,
@@ -73,7 +84,7 @@ INPUT_ARG_PARSER.add_argument(
 )
 
 
-def _run(satellite_dir_name, years, num_satellite_files,
+def _run(satellite_dir_name, years, cyclone_id_strings, num_satellite_files,
          num_values_per_high_res_channel, num_values_per_low_res_channel,
          output_file_name):
     """Computes normalization parameters.
@@ -82,25 +93,34 @@ def _run(satellite_dir_name, years, num_satellite_files,
 
     :param satellite_dir_name: See documentation at top of file.
     :param years: Same.
+    :param cyclone_id_strings: Same.
     :param num_satellite_files: Same.
     :param num_values_per_high_res_channel: Same.
     :param num_values_per_low_res_channel: Same.
     :param output_file_name: Same.
     """
 
+    if len(years) == 1 and years[0] < 1900:
+        years = None
+    if len(cyclone_id_strings) == 1 and cyclone_id_strings[0] == '':
+        cyclone_id_strings = None
+
+    assert not (years is None and cyclone_id_strings is None)
     error_checking.assert_is_geq(num_satellite_files, 10)
 
-    cyclone_id_strings = satellite_io.find_cyclones(
-        directory_name=satellite_dir_name, raise_error_if_all_missing=True
-    )
-    cyclone_years = numpy.array(
-        [misc_utils.parse_cyclone_id(c)[0] for c in cyclone_id_strings],
-        dtype=int
-    )
+    if cyclone_id_strings is None:
+        cyclone_id_strings = satellite_io.find_cyclones(
+            directory_name=satellite_dir_name, raise_error_if_all_missing=True
+        )
+        cyclone_years = numpy.array(
+            [misc_utils.parse_cyclone_id(c)[0] for c in cyclone_id_strings],
+            dtype=int
+        )
 
-    good_flags = numpy.array([c in years for c in cyclone_years], dtype=float)
-    good_indices = numpy.where(good_flags)[0]
-    cyclone_id_strings = [cyclone_id_strings[k] for k in good_indices]
+        good_flags = numpy.array([c in years for c in cyclone_years], dtype=float)
+        good_indices = numpy.where(good_flags)[0]
+        cyclone_id_strings = [cyclone_id_strings[k] for k in good_indices]
+
     cyclone_id_strings.sort()
 
     satellite_file_names_list_list = [
@@ -145,6 +165,7 @@ if __name__ == '__main__':
     _run(
         satellite_dir_name=getattr(INPUT_ARG_OBJECT, INPUT_DIR_ARG_NAME),
         years=numpy.array(getattr(INPUT_ARG_OBJECT, YEARS_ARG_NAME), dtype=int),
+        cyclone_id_strings=getattr(INPUT_ARG_OBJECT, CYCLONE_IDS_ARG_NAME),
         num_satellite_files=getattr(INPUT_ARG_OBJECT, NUM_FILES_ARG_NAME),
         num_values_per_high_res_channel=getattr(
             INPUT_ARG_OBJECT, NUM_VALUES_PER_HIGH_RES_ARG_NAME
