12d383f8831d1b651b3cd7173cc29ed14da73cd7
thunderhoser <lagerqui@ualberta.ca>
2024-01-08 21:24:16 -0700

    FOO

--- ml4tccf/slurm/paper_experiment02_ensemble/isotonic_regression/dumb_ensemble/testing/windfall_new_enviro/split_predictions_by_storm_type.sh
+++ ml4tccf/slurm/paper_experiment02_ensemble/isotonic_regression/dumb_ensemble/testing/windfall_new_enviro/split_predictions_by_storm_type.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CODE_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_standalone/ml4tccf"
-TOP_MODEL_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_models/paper_ensemble/tropical_and_nontropical"
+TOP_MODEL_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_models/paper_experiment02_ensemble/tropical_and_nontropical"
 A_DECK_FILE_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_project/a_decks/including_western_pacific_a-decks_not-b-decks/processed_a_decks_2016-2022.nc"
 
 model_dir_name="${TOP_MODEL_DIR_NAME}/dumb_ensemble"
