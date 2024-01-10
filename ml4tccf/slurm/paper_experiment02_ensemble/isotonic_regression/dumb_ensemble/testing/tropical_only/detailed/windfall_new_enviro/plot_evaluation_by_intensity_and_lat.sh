dee6e1c9fa20fae670cbd9c3e88f0d8130a8499d
thunderhoser <lagerqui@ualberta.ca>
2024-01-10 11:29:16 -0700

    FOO

--- ml4tccf/slurm/paper_experiment02_ensemble/isotonic_regression/dumb_ensemble/testing/tropical_only/detailed/windfall_new_enviro/plot_evaluation_by_intensity_and_lat.sh
+++ ml4tccf/slurm/paper_experiment02_ensemble/isotonic_regression/dumb_ensemble/testing/tropical_only/detailed/windfall_new_enviro/plot_evaluation_by_intensity_and_lat.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-eval_file_names_string=$1
-output_dir_name=$2
+max_wind_cutoffs_string=$1
+eval_file_names_string=$2
+output_dir_name=$3
 
 CODE_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_standalone/ml4tccf"
 
 python3 -u "${CODE_DIR_NAME}/plot_evaluation_by_intensity.py" \
---max_wind_cutoffs_kt 50 80 \
+--max_wind_cutoffs_kt ${max_wind_cutoffs_string} \
 --tc_center_latitude_cutoffs_deg_n 15 25 35 45 \
 --input_max_wind_based_eval_file_names ${eval_file_names_string} \
 --label_font_size=40 \
