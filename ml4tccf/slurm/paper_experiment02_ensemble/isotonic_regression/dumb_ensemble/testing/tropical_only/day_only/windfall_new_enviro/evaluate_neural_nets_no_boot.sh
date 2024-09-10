029b55dfff4a2dc9d180ba00459c9f0c224ce4c4
thunderhoser <lagerqui@ualberta.ca>
2024-09-10 12:09:53 -0600

    FOO

---
+++ ml4tccf/slurm/paper_experiment02_ensemble/isotonic_regression/dumb_ensemble/testing/tropical_only/day_only/windfall_new_enviro/evaluate_neural_nets_no_boot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CODE_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_standalone/ml4tccf"
+TOP_MODEL_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml4tccf_models/paper_experiment02_ensemble/tropical_and_nontropical"
+
+python3 -u "${CODE_DIR_NAME}/evaluate_neural_net.py" \
+--input_prediction_file_pattern="${TOP_MODEL_DIR_NAME}/dumb_ensemble/isotonic_regression/testing_by_storm_type/storm-type=tropical/testing_by_day_night/day/predictions*.nc" \
+--num_bootstrap_reps=1 \
+--num_xy_offset_bins=100 \
+--xy_offset_limits_metres -100000 100000 \
+--num_offset_distance_bins=50 \
+--offset_distance_limits_metres 0 100000 \
+--num_offset_direction_bins=36 \
+--output_file_name="${TOP_MODEL_DIR_NAME}/dumb_ensemble/isotonic_regression/testing_by_storm_type/storm-type=tropical/testing_by_day_night/day/evaluation_no_boot.nc"
