2903a71f2005f4ece0e09cf0a88b4c81c5bdba98
thunderhoser <lagerqui@ualberta.ca>
2024-10-10 11:48:15 -0600

    FOO

---
+++ ml4tccf/bash_scripts/process_satellite_data/process_2024AL09.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+conda activate base
+
+CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf"
+INPUT_DIR_NAME="/mnt/shceph/users2/tccdb/nrt_geocenter/g16/0.0.7"
+OUTPUT_DIR_NAME="/mnt/nvme-data5/lagerquist/ml4tccf_project/satellite_data/processed"
+
+CYCLONE_ID_STRING="2024AL09"
+
+python3 -u "${CODE_DIR_NAME}/process_satellite_data.py" \
+--input_dir_name="${INPUT_DIR_NAME}" \
+--cyclone_id_string="${CYCLONE_ID_STRING}" \
+--max_bad_pixels_low_res=6250 \
+--max_bad_pixels_high_res=25000 \
+--temporary_dir_name="${OUTPUT_DIR_NAME}/temporary_${CYCLONE_ID_STRING}" \
+--min_visible_pixel_fraction=0.9 \
+--altitude_angle_exe_name="/home/lagerquist/solarpos/solarpos" \
+--output_dir_name="${OUTPUT_DIR_NAME}"
