82e2f51303c783d90ff5f9fec694e1d1bef4518a
thunderhoser <lagerqui@ualberta.ca>
2024-10-14 11:35:02 -0600

    Added script convert_prediction_file_for_zhixing.py.

--- ml4tccf/scripts/convert_prediction_file_for_zhixing.py
+++ ml4tccf/scripts/convert_prediction_file_for_zhixing.py
@@ -196,7 +196,7 @@ def _run(input_prediction_file_name, satellite_dir_name,
         _convert_predictions_one_sample(
             prediction_table_xarray=prediction_table_xarray,
             cyclone_id_string=unique_id_time_matrix[i, 0],
-            target_time_unix_sec=unique_id_time_matrix[i, 1],
+            target_time_unix_sec=int(unique_id_time_matrix[i, 1]),
             satellite_dir_name=satellite_dir_name,
             output_prediction_dir_name=output_prediction_dir_name
         )
