c4e492ad1a97101def84c56660fb5b110b1b6183
thunderhoser <lagerqui@ualberta.ca>
2024-10-16 11:41:34 -0600

    FOO

--- ml4tccf/bash_scripts/apply_models/isotonic_regression/create_multimodel_ensemble/convert_for_zhixing/compare_with_zhixing/compare_with_zhixing_2024AL93.sh
+++ ml4tccf/bash_scripts/apply_models/isotonic_regression/create_multimodel_ensemble/convert_for_zhixing/compare_with_zhixing/compare_with_zhixing_2024AL93.sh
@@ -5,7 +5,7 @@ conda activate base
 CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
 
 CYCLONE_ID_STRING="2024AL93"
-FAKE_CYCLONE_ID_STRING="al932014"
+FAKE_CYCLONE_ID_STRING="al932024"
 ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
 
 log_file_name="compare_with_zhixing_${CYCLONE_ID_STRING}.out"
--- ml4tccf/bash_scripts/apply_models/isotonic_regression/create_multimodel_ensemble/convert_for_zhixing/compare_with_zhixing/compare_with_zhixing_2024EP99.sh
+++ ml4tccf/bash_scripts/apply_models/isotonic_regression/create_multimodel_ensemble/convert_for_zhixing/compare_with_zhixing/compare_with_zhixing_2024EP99.sh
@@ -5,7 +5,7 @@ conda activate base
 CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
 
 CYCLONE_ID_STRING="2024EP99"
-FAKE_CYCLONE_ID_STRING="ep992014"
+FAKE_CYCLONE_ID_STRING="ep992024"
 ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
 
 log_file_name="compare_with_zhixing_${CYCLONE_ID_STRING}_${valid_date_string}.out"
