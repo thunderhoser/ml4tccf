b006326c30dab1d5fd9a96c256989fbd01deac37
thunderhoser <lagerqui@ualberta.ca>
2024-10-16 11:26:35 -0600

    FOO

--- ml4tccf/scripts/plot_ryan_zhixing_comparison.py
+++ ml4tccf/scripts/plot_ryan_zhixing_comparison.py
@@ -17,22 +17,26 @@ from ml4tccf.io import border_io
 from ml4tccf.utils import misc_utils
 from ml4tccf.plotting import plotting_utils
 
-# TODO(thunderhoser): Get prelim best track in here somehow.
 HOURS_TO_SECONDS = 3600
-COLOUR_BAR_TIME_FORMAT = '%H %b %d'
+COLOUR_BAR_TIME_FORMAT = '%HZ %b %-d'
 
 DATE_PATTERN_STRING = '[0-9][0-9][0-9][0-9][0-1][0-9][0-3][0-9]'
 HOUR_MINUTE_PATTERN_STRING = '[0-2][0-9][0-5][0-9]'
 
-RYAN_MARKER_SIZE = 30
+RYAN_MARKER_SIZE = 36
 RYAN_MARKER_TYPE = 's'
 RYAN_MARKER_EDGE_WIDTH = 1.5
 RYAN_MARKER_EDGE_COLOUR = numpy.full(3, 0.)
 
-ZHIXING_MARKER_SIZE = 30
+ZHIXING_MARKER_SIZE = 36
 ZHIXING_MARKER_TYPE = 'o'
 ZHIXING_MARKER_EDGE_WIDTH = 1.5
 ZHIXING_MARKER_EDGE_COLOUR = numpy.full(3, 152. / 255)
+
+BEST_TRACK_MARKER_SIZE = 60
+BEST_TRACK_MARKER_TYPE = '*'
+BEST_TRACK_MARKER_EDGE_WIDTH = 0.
+BEST_TRACK_MARKER_EDGE_COLOUR = numpy.full(3, 152. / 255)
 
 TICK_LABEL_FONT_SIZE = 40
 COLOUR_MAP_OBJECT = pyplot.get_cmap('gist_ncar')
@@ -43,6 +47,7 @@ FIGURE_RESOLUTION_DPI = 300
 
 RYAN_DIR_ARG_NAME = 'input_ryan_dir_name'
 ZHIXING_DIR_ARG_NAME = 'input_zhixing_dir_name'
+RAW_BT_FILE_ARG_NAME = 'input_raw_best_track_file_name'
 CYCLONE_ID_ARG_NAME = 'cyclone_id_string'
 OUTPUT_FILE_ARG_NAME = 'output_file_name'
 
@@ -55,6 +60,10 @@ ZHIXING_DIR_HELP_STRING = (
     'Path to directory with estimates from Zhixing -- stored as CSV files with '
     'names like "<bbnnyyyy>_<yyyymmdd>_<HHMM>.txt", where <bbnnyyyy> is the '
     'cyclone ID.'
+)
+RAW_BT_FILE_HELP_STRING = (
+    'Path to file with raw best tracks for the same cyclone.  Should be named '
+    'like "b<bbnnyyyy>.dat".'
 )
 CYCLONE_ID_HELP_STRING = 'Cyclone ID in the usual format (yyyynnBB).'
 OUTPUT_FILE_HELP_STRING = (
@@ -71,6 +80,10 @@ INPUT_ARG_PARSER.add_argument(
     help=ZHIXING_DIR_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
+    '--' + RAW_BT_FILE_ARG_NAME, type=str, required=True,
+    help=RAW_BT_FILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
     '--' + CYCLONE_ID_ARG_NAME, type=str, required=True,
     help=CYCLONE_ID_HELP_STRING
 )
@@ -80,13 +93,85 @@ INPUT_ARG_PARSER.add_argument(
 )
 
 
-def _run(ryan_dir_name, zhixing_dir_name, cyclone_id_string, output_file_name):
+def _read_raw_best_track_file(csv_file_name, cyclone_id_string):
+    """Reads raw best tracks from CSV file.
+
+    P = number of points in track
+
+    :param csv_file_name: Path to input file.
+    :param cyclone_id_string: ID of expected cyclone.
+    :return: bt_latitudes_deg_n: length-P numpy array of latitudes (deg north).
+    :return: bt_longitudes_deg_e: length-P numpy array of longitudes (deg east).
+    :return: bt_times_unix_sec: length-P numpy array of times.
+    """
+
+    year, basin_id_string, cyclone_number = misc_utils.parse_cyclone_id(
+        cyclone_id_string
+    )
+
+    csv_file_handle = open(csv_file_name, 'r')
+    bt_latitudes_deg_n = []
+    bt_longitudes_deg_e = []
+    bt_times_unix_sec = []
+
+    for this_line in csv_file_handle.readlines():
+        these_words = this_line.split(',')
+        these_words = [w.strip() for w in these_words]
+
+        assert these_words[0].upper() == basin_id_string
+        assert int(these_words[1]) == cyclone_number
+        assert these_words[4].upper() == 'BEST'
+
+        this_time_unix_sec = time_conversion.string_to_unix_sec(
+            these_words[2], '%Y%m%d%H'
+        )
+
+        this_latitude_string = these_words[6]
+        assert (
+            this_latitude_string.endswith('N') or
+            this_latitude_string.endswith('S')
+        )
+        this_latitude_deg_n = int(this_latitude_string[:-1])
+        if this_latitude_string.endswith('S'):
+            this_latitude_deg_n *= -1.
+
+        this_longitude_string = these_words[7]
+        assert (
+            this_longitude_string.endswith('E') or
+            this_longitude_string.endswith('W')
+        )
+        this_longitude_deg_e = int(this_longitude_string[:-1])
+        if this_longitude_string.endswith('W'):
+            this_longitude_deg_e *= -1.
+
+        bt_latitudes_deg_n.append(this_latitude_deg_n)
+        bt_longitudes_deg_e.append(this_longitude_deg_e)
+        bt_times_unix_sec.append(this_time_unix_sec)
+
+    bt_latitudes_deg_n = numpy.array(bt_latitudes_deg_n, dtype=float)
+    error_checking.assert_is_valid_lat_numpy_array(
+        bt_latitudes_deg_n, allow_nan=False
+    )
+
+    bt_longitudes_deg_e = numpy.array(bt_longitudes_deg_e, dtype=float)
+    bt_longitudes_deg_e = lng_conversion.convert_lng_negative_in_west(
+        bt_longitudes_deg_e, allow_nan=False
+    )
+
+    bt_times_unix_sec = numpy.array(bt_times_unix_sec, dtype=int)
+
+    return bt_latitudes_deg_n, bt_longitudes_deg_e, bt_times_unix_sec
+
+
+def _run(ryan_dir_name, zhixing_dir_name, raw_best_track_file_name,
+         cyclone_id_string, output_file_name):
     """Plots comparison of estimated tracks from Ryan and Zhixing.
 
     This is effectively the main method.
 
     :param ryan_dir_name: See documentation at top of this script.
     :param zhixing_dir_name: Same.
+    :param raw_best_track_file_name: Same.
     :param cyclone_id_string: Same.
     :param output_file_name: Same.
     """
@@ -199,6 +284,14 @@ def _run(ryan_dir_name, zhixing_dir_name, cyclone_id_string, output_file_name):
         zhixing_longitudes_deg_e, allow_nan=False
     )
 
+    print('Reading data from: "{0:s}"...'.format(raw_best_track_file_name))
+    bt_latitudes_deg_n, bt_longitudes_deg_e, bt_times_unix_sec = (
+        _read_raw_best_track_file(
+            csv_file_name=raw_best_track_file_name,
+            cyclone_id_string=cyclone_id_string
+        )
+    )
+
     figure_object, axes_object = pyplot.subplots(
         1, 1, figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
     )
@@ -214,11 +307,13 @@ def _run(ryan_dir_name, zhixing_dir_name, cyclone_id_string, output_file_name):
 
     min_time_unix_sec = min([
         numpy.min(ryan_times_unix_sec),
-        numpy.min(zhixing_times_unix_sec)
+        numpy.min(zhixing_times_unix_sec),
+        numpy.min(bt_times_unix_sec)
     ])
     max_time_unix_sec = max([
         numpy.max(ryan_times_unix_sec),
-        numpy.max(zhixing_times_unix_sec)
+        numpy.max(zhixing_times_unix_sec),
+        numpy.max(bt_times_unix_sec)
     ])
     colour_norm_object = pyplot.Normalize(
         vmin=min_time_unix_sec, vmax=max_time_unix_sec
@@ -239,12 +334,19 @@ def _run(ryan_dir_name, zhixing_dir_name, cyclone_id_string, output_file_name):
         cmap=COLOUR_MAP_OBJECT,
         norm=colour_norm_object
     )
+    axes_object.scatter(
+        x=bt_longitudes_deg_e, y=bt_latitudes_deg_n,
+        s=BEST_TRACK_MARKER_SIZE, marker=BEST_TRACK_MARKER_TYPE,
+        linewidths=BEST_TRACK_MARKER_EDGE_WIDTH,
+        edgecolors=BEST_TRACK_MARKER_EDGE_COLOUR,
+        c=bt_times_unix_sec, cmap=COLOUR_MAP_OBJECT, norm=colour_norm_object
+    )
 
     all_latitudes_deg_n = numpy.concatenate(
-        [ryan_latitudes_deg_n, zhixing_latitudes_deg_n]
+        [ryan_latitudes_deg_n, zhixing_latitudes_deg_n, bt_latitudes_deg_n]
     )
     all_longitudes_deg_e = numpy.concatenate(
-        [ryan_longitudes_deg_e, zhixing_longitudes_deg_e]
+        [ryan_longitudes_deg_e, zhixing_longitudes_deg_e, bt_longitudes_deg_e]
     )
     plotting_utils.plot_grid_lines(
         plot_latitudes_deg_n=all_latitudes_deg_n,
@@ -287,7 +389,7 @@ def _run(ryan_dir_name, zhixing_dir_name, cyclone_id_string, output_file_name):
     )
 
     all_times_unix_sec = numpy.concatenate([
-        ryan_times_unix_sec, zhixing_times_unix_sec
+        ryan_times_unix_sec, zhixing_times_unix_sec, bt_times_unix_sec
     ])
     tick_times_unix_sec = time_periods.range_and_interval_to_list(
         start_time_unix_sec=numpy.min(all_times_unix_sec),
@@ -317,6 +419,9 @@ if __name__ == '__main__':
     _run(
         ryan_dir_name=getattr(INPUT_ARG_OBJECT, RYAN_DIR_ARG_NAME),
         zhixing_dir_name=getattr(INPUT_ARG_OBJECT, ZHIXING_DIR_ARG_NAME),
+        raw_best_track_file_name=getattr(
+            INPUT_ARG_OBJECT, RAW_BT_FILE_ARG_NAME
+        ),
         cyclone_id_string=getattr(INPUT_ARG_OBJECT, CYCLONE_ID_ARG_NAME),
         output_file_name=getattr(INPUT_ARG_OBJECT, OUTPUT_FILE_ARG_NAME)
     )
