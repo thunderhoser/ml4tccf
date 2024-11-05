c5f6cd5a6930d2057f39d6447f2f8b7843e14c06
thunderhoser <lagerqui@ualberta.ca>
2024-11-05 08:23:14 -0700

    FOO

---
+++ ml4tccf/bash_scripts/apply_models_real_time_short_track/isotonic_regression/create_multimodel_ensemble/convert_for_zhixing/convert_for_zhixing_2024AL09.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+conda activate base
+
+CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
+SATELLITE_DIR_NAME="/mnt/nvme-data5/lagerquist/ml4tccf_project/satellite_data/processed/normalized_params_for_paper/simplified_700x700"
+A_DECK_FILE_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/a_decks/processed/a_decks_2024_normalized.nc"
+
+CYCLONE_ID_STRING="2024AL09"
+VALID_DATE_STRINGS=("20240924" "20240925" "20240926" "20240927" "20240928" "20240929")
+ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
+
+for valid_date_string in "${VALID_DATE_STRINGS[@]}"; do
+    log_file_name="convert_for_zhixing_${CYCLONE_ID_STRING}_${valid_date_string}.out"
+
+    python3 -u "${CODE_DIR_NAME}/convert_prediction_file_for_zhixing.py" &> ${log_file_name} \
+    --input_prediction_file_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc" \
+    --input_satellite_dir_name="/mnt/nvme-data5/lagerquist/ml4tccf_project/satellite_data/processed/normalized_params_for_paper/simplified_700x700" \
+    --output_prediction_dir_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_short_track/isotonic_regression/for_zhixing"
+done
---
+++ ml4tccf/bash_scripts/apply_models_real_time_short_track/isotonic_regression/create_multimodel_ensemble/create_ensemble_2024AL12.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+conda activate base
+
+CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
+SATELLITE_DIR_NAME="/mnt/nvme-data5/lagerquist/ml4tccf_project/satellite_data/processed/normalized_params_for_paper/simplified_700x700"
+A_DECK_FILE_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/a_decks/processed/a_decks_2024_normalized.nc"
+
+MODEL_DIR_NAMES=("/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/wavelengths-microns=3.900-6.185-6.950" "/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/wavelengths-microns=3.900-7.340-13.300" "/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/wavelengths-microns=6.950-10.350-11.200" "/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/wavelengths-microns=8.500-9.610-12.300")
+MODEL_DESCRIPTION_STRINGS=("3.900-6.185-6.950" "3.900-7.340-13.300" "6.950-10.350-11.200" "8.500-9.610-12.300")
+
+CYCLONE_ID_STRING="2024AL12"
+VALID_DATE_STRINGS=("20240929" "20240930" "20241001" "20241002" "20241003" "20241004" "20241005" "20241006" "20241007" "20241008")
+ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
+
+for valid_date_string in "${VALID_DATE_STRINGS[@]}"; do
+    log_file_name="create_ensemble_${CYCLONE_ID_STRING}_${valid_date_string}.out"
+
+    python3 -u "${CODE_DIR_NAME}/create_multimodel_ensemble.py" &> ${log_file_name} \
+    --input_prediction_file_names "${MODEL_DIR_NAMES[0]}/real_time_predictions_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc" "${MODEL_DIR_NAMES[1]}/real_time_predictions_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc" "${MODEL_DIR_NAMES[2]}/real_time_predictions_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc" "${MODEL_DIR_NAMES[3]}/real_time_predictions_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc" \
+    --max_total_ensemble_size=10000 \
+    --output_prediction_file_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc"
+done
