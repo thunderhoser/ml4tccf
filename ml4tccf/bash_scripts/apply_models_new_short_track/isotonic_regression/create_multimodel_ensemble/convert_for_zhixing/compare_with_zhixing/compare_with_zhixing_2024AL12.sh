8bbb2ab35511f27690ba36e43089a4190267844a
thunderhoser <lagerqui@ualberta.ca>
2024-11-07 12:24:02 -0700

    FOO

---
+++ ml4tccf/bash_scripts/apply_models_new_short_track/isotonic_regression/create_multimodel_ensemble/convert_for_zhixing/compare_with_zhixing/compare_with_zhixing_2024AL12.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+conda activate base
+
+CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
+
+CYCLONE_ID_STRING="2024AL12"
+FAKE_CYCLONE_ID_STRING="al122024"
+ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
+
+log_file_name="compare_with_zhixing_${CYCLONE_ID_STRING}.out"
+
+python3 -u "${CODE_DIR_NAME}/plot_ryan_zhixing_comparison.py" &> ${log_file_name} \
+--input_ryan_dir_name="${ENSEMBLE_DIR_NAME}/predictions_new_short_track/isotonic_regression/for_zhixing/${FAKE_CYCLONE_ID_STRING}" \
+--input_zhixing_dir_name="/mnt/shnas10/users/zxruan/nrt_geocenter/preds/best_estimate_center/${FAKE_CYCLONE_ID_STRING}" \
+--cyclone_id_string="${CYCLONE_ID_STRING}" \
+--input_raw_best_track_file_name="/mnt/shceph/data/ATCF/atcf_rt/ATCF/dat/NHC/b${FAKE_CYCLONE_ID_STRING}.dat" \
+--output_file_name="${ENSEMBLE_DIR_NAME}/predictions_new_short_track/isotonic_regression/comparisons_with_zhixing/${CYCLONE_ID_STRING}.jpg"
+
+python3 -u "${CODE_DIR_NAME}/plot_ryan_zhixing_comparison.py" &>> ${log_file_name} \
+--input_ryan_dir_name="${ENSEMBLE_DIR_NAME}/predictions_new_short_track/isotonic_regression/for_zhixing/${FAKE_CYCLONE_ID_STRING}" \
+--input_zhixing_dir_name="" \
+--cyclone_id_string="${CYCLONE_ID_STRING}" \
+--input_raw_best_track_file_name="/mnt/shceph/data/ATCF/atcf_rt/ATCF/dat/NHC/b${FAKE_CYCLONE_ID_STRING}.dat" \
+--output_file_name="${ENSEMBLE_DIR_NAME}/predictions_new_short_track/isotonic_regression/comparisons_with_zhixing/${CYCLONE_ID_STRING}_ryan_only.jpg"
+
+python3 -u "${CODE_DIR_NAME}/plot_ryan_zhixing_comparison.py" &>> ${log_file_name} \
+--input_ryan_dir_name="" \
+--input_zhixing_dir_name="/mnt/shnas10/users/zxruan/nrt_geocenter/preds/best_estimate_center/${FAKE_CYCLONE_ID_STRING}" \
+--cyclone_id_string="${CYCLONE_ID_STRING}" \
+--input_raw_best_track_file_name="/mnt/shceph/data/ATCF/atcf_rt/ATCF/dat/NHC/b${FAKE_CYCLONE_ID_STRING}.dat" \
+--output_file_name="${ENSEMBLE_DIR_NAME}/predictions_new_short_track/isotonic_regression/comparisons_with_zhixing/${CYCLONE_ID_STRING}_zhixing_only.jpg"
