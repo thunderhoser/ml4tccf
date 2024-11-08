f9c27e0c087edd91ecbfa8a2bee8ab8bc15772e2
thunderhoser <lagerqui@ualberta.ca>
2024-11-07 20:37:07 -0700

    FOO

--- ml4tccf/bash_scripts/apply_models_real_time_best_track/isotonic_regression/create_multimodel_ensemble/plot_predictions_2024AL12_second.sh
+++ ml4tccf/bash_scripts/apply_models_real_time_best_track/isotonic_regression/create_multimodel_ensemble/plot_predictions_2024AL12_second.sh
@@ -8,13 +8,13 @@ SATELLITE_NORM_FILE_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/ir_satel
 
 ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
 
-log_file_name="plot_predictions_2024AL12.out"
+log_file_name="plot_predictions_2024AL12_second.out"
 
 python3 -u "${CODE_DIR_NAME}/waf2024_plot_predictions.py" &> ${log_file_name} \
 --input_prediction_file_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_best_track/isotonic_regression/2024AL12_20241006.nc" \
 --input_satellite_dir_name="${SATELLITE_DIR_NAME}" \
 --input_normalization_file_name="${SATELLITE_NORM_FILE_NAME}" \
---target_time_strings "2024-10-06-1810" "2024-10-06-1710" "2024-10-06-1740" "2024-10-06-1840" "2024-10-06-1910" \
+--target_time_strings "2024-10-06-2300" "2024-10-06-2330" \
 --num_samples_per_target_time=1 \
 --lag_times_minutes 0 \
 --wavelengths_microns 3.9  6.185  6.95  7.34  8.5  9.61  10.35  11.2  12.3  13.3 \
@@ -23,3 +23,17 @@ python3 -u "${CODE_DIR_NAME}/waf2024_plot_predictions.py" &> ${log_file_name} \
 --prob_contour_smoothing_radius_px=2 \
 --prob_contour_opacity=0.7 \
 --output_dir_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_best_track/isotonic_regression/2024AL12_20241006"
+
+python3 -u "${CODE_DIR_NAME}/waf2024_plot_predictions.py" &> ${log_file_name} \
+--input_prediction_file_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_best_track/isotonic_regression/2024AL12_20241007.nc" \
+--input_satellite_dir_name="${SATELLITE_DIR_NAME}" \
+--input_normalization_file_name="${SATELLITE_NORM_FILE_NAME}" \
+--target_time_strings "2024-10-07-0000" "2024-10-07-0030" "2024-10-07-0500" "2024-10-07-0530" "2024-10-07-0600" "2024-10-07-0630" "2024-10-07-0700" \
+--num_samples_per_target_time=1 \
+--lag_times_minutes 0 \
+--wavelengths_microns 3.9  6.185  6.95  7.34  8.5  9.61  10.35  11.2  12.3  13.3 \
+--prediction_plotting_format_string="probability_contours" \
+--prob_colour_map_name="BuGn" \
+--prob_contour_smoothing_radius_px=2 \
+--prob_contour_opacity=0.7 \
+--output_dir_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_best_track/isotonic_regression/2024AL12_20241007"
