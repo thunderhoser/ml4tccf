e833e9e2ecec394980968cc3b84aa5fdcdc469e8
thunderhoser <lagerqui@ualberta.ca>
2024-11-19 12:23:43 -0700

    FOO

---
+++ ml4tccf/bash_scripts/apply_models_real_time_new_short_track/isotonic_regression/create_multimodel_ensemble/plot_predictions_nice_2024AL12_20241002.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+conda activate base
+
+CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/waf2024"
+SATELLITE_DIR_NAME="/mnt/nvme-data5/lagerquist/ml4tccf_project/satellite_data/processed/normalized_params_for_paper/recentered_on_short_track/simplified_700x700"
+SATELLITE_NORM_FILE_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/ir_satellite_normalization_params.zarr"
+
+CYCLONE_ID_STRING="2024AL12"
+VALID_DATE_STRINGS=("20241002")
+ENSEMBLE_DIR_NAME="/mnt/shnas10/users/lagerquist/ml4tccf_project/geocenter_models/ensemble"
+
+for valid_date_string in "${VALID_DATE_STRINGS[@]}"; do
+    log_file_name="plot_predictions_nice_${CYCLONE_ID_STRING}_${valid_date_string}.out"
+    
+    python3 -u "${CODE_DIR_NAME}/wpo2025_plot_predictions.py" &> ${log_file_name} \
+    --input_prediction_file_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_new_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}.nc" \
+    --input_satellite_dir_name="${SATELLITE_DIR_NAME}" \
+    --input_normalization_file_name="${SATELLITE_NORM_FILE_NAME}" \
+    --input_best_track_dir_name="/mnt/shnas10/users/galka/track_interpolated/best_track/bal122024" \
+    --num_samples_per_target_time=1 \
+    --lag_times_minutes 0 \
+    --wavelengths_microns 3.9 6.95 10.35 \
+    --prediction_plotting_format_string="probability_contours" \
+    --prob_colour_map_name="BuGn" \
+    --prob_contour_smoothing_radius_px=2 \
+    --prob_contour_opacity=0.7 \
+    --output_dir_name="${ENSEMBLE_DIR_NAME}/real_time_predictions_new_short_track/isotonic_regression/${CYCLONE_ID_STRING}_${valid_date_string}"
+done
