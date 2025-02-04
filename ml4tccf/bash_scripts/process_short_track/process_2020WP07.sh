e7879e5234e0a20b88552f74e90116153ced63a5
thunderhoser <lagerqui@ualberta.ca>
2025-02-04 13:53:22 -0700

    FOO

---
+++ ml4tccf/bash_scripts/process_short_track/process_2020WP07.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+conda activate base
+
+CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
+INPUT_DIR_NAME="/mnt/shnas10/data/SHORT_TRACK"
+OUTPUT_DIR_NAME="/mnt/nvme-data5/lagerquist/ml4tccf_project/short_track_data/processed"
+
+CYCLONE_ID_STRING="2020WP07"
+LOG_FILE_NAME="process_short_track_data_${CYCLONE_ID_STRING}.out"
+
+python3 -u "${CODE_DIR_NAME}/process_short_track_data.py" &> ${LOG_FILE_NAME} \
+--input_raw_dir_name="${INPUT_DIR_NAME}" \
+--cyclone_id_string="${CYCLONE_ID_STRING}" \
+--num_minutes_back=360 \
+--num_minutes_ahead=350 \
+--output_processed_dir_name="${OUTPUT_DIR_NAME}"
