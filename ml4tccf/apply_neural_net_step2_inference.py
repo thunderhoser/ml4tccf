a2a155539814ab8b38ea2f66f5ba4ef437c3ef51
thunderhoser <lagerqui@ualberta.ca>
2025-03-25 11:56:49 -0600

    FOO

--- ml4tccf/apply_neural_net_step2_inference.py
+++ ml4tccf/apply_neural_net_step2_inference.py
@@ -197,10 +197,22 @@ def _run(model_file_name, example_dir_name, cyclone_id_string, output_dir_name):
         ].values
 
     print(SEPARATOR_STRING)
-    prediction_matrix = nn_utils.apply_model(
-        model_object=model_object, predictor_matrices=predictor_matrices,
-        num_examples_per_batch=NUM_EXAMPLES_PER_BATCH, verbose=True
-    )
+
+    try:
+        prediction_matrix = nn_utils.apply_model(
+            model_object=model_object,
+            predictor_matrices=predictor_matrices,
+            num_examples_per_batch=NUM_EXAMPLES_PER_BATCH,
+            verbose=True
+        )
+    except:
+        prediction_matrix = nn_utils.apply_model(
+            model_object=model_object,
+            predictor_matrices=[predictor_matrices[0]],
+            num_examples_per_batch=NUM_EXAMPLES_PER_BATCH,
+            verbose=True
+        )
+
     print(SEPARATOR_STRING)
     del predictor_matrices
 
