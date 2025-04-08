8d7864bab00f146cea47eab61e1ffdc24de563ac
thunderhoser <lagerqui@ualberta.ca>
2025-04-08 10:40:34 -0600

    FOO

--- ml4tccf/bash_scripts/process_satellite_data/new_storms/process_2024AL09.sh
+++ ml4tccf/bash_scripts/process_satellite_data/new_storms/process_2024AL09.sh
@@ -3,10 +3,10 @@
 conda activate base
 
 CODE_DIR_NAME="/home/lagerquist/ml4tccf/ml4tccf/scripts"
-INPUT_DIR_NAME="/mnt/shceph/users2/tccdb/nrt_geocenter/ahi9/0.0.7/2024"
+INPUT_DIR_NAME="/mnt/shceph/users2/tccdb/nrt_geocenter/g16/0.0.7/2024"
 OUTPUT_DIR_NAME="/mnt/nvme-data5/lagerquist/ml4tccf_project/satellite_data/processed"
 
-CYCLONE_ID_STRING="2024WP18"
+CYCLONE_ID_STRING="2024AL09"
 LOG_FILE_NAME="process_satellite_data_${CYCLONE_ID_STRING}.out"
 
 python3 -u "${CODE_DIR_NAME}/process_satellite_data.py" &> ${LOG_FILE_NAME} \
